e0141f5fcb7e2807dcecf3099e1feb477fc6377d
jangjo123 <63893284+jangjo123@users.noreply.github.com>
2021-03-29 20:27:04 +0900

    캐릭터 생성 느낌만 내기

---
+++ Assets/Script/CreateDefenser.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreateDefenser : MonoBehaviour
+{
+    private GameObject Soldier;
+    
+    
+    void Start()
+    {
+      gameObject.SetActive(true);
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    public void create()
+    {
+        int randomnumber = Random.Range(0, 4);
+        Soldier = transform.GetChild(randomnumber).gameObject;
+
+        Soldier.transform.position = new Vector3(-Random.Range(20, 40), 0, Random.Range(90, 110));
+        Soldier.SetActive(true);
+    }
+}

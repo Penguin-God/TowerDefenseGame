d0c2171b818b5bb5f576f93504462988f94e6e5e
Penguin-God <gkswh4860@naver.com>
2021-04-03 14:23:28 +0900

    Stage Manage

--- Assets/Script/EnemySpaw.cs
+++ Assets/Script/EnemySpaw.cs
@@ -4,45 +4,56 @@ using UnityEngine;
 
 public class EnemySpaw : MonoBehaviour
 {
-    public GameObject enemyPrefab;
-    private int Stagenumber;
-    private int EnemyCount;
-    public int startEnemyCount;
-    private UIManager UIManager;
+    public GameObject[] enemyPrefab; // 0 : 아처, 1 : 마법사, 2 : 창병, 3 : 검사
+    public int stageNumber;
+    //public int startEnemyCount;
+    //private int EnemyCount;
+    Dictionary<int, int[]> stageDictionary; // 0 : 소환할 enemy, 1 : enemy 수, 2 : 대기 시간 
 
-    private void Start()
+    private void Awake()
     {
-        EnemyCount = startEnemyCount;
-        Stagenumber = 1;
-        
+        stageDictionary = new Dictionary<int, int[]>();
+        stageDictionary.Add(1, new int[] { 0, 8, 2});
+        stageDictionary.Add(2, new int[] { 1, 8, 2 });
+        stageDictionary.Add(3, new int[] { 2, 8, 1 });
+        NextStage();
     }
 
-    private void Update()
-    {
-        if (Stagenumber + 7 == EnemyCount)
-        {
-            Stagenumber += 1;
-            StartCoroutine(Stage(enemyPrefab, EnemyCount, 2));
-        }
-    }
+    //void Update()
+    //{
+    //    if (stageNumber + 7 == EnemyCount)
+    //    {
+    //        stageNumber += 1;
+    //        StartCoroutine(StageCoroutine(0, EnemyCount, 2));
+    //    }
+    //}
 
-    IEnumerator Stage(GameObject instantEnemy, int enemyCount, int waitTime)
+    IEnumerator StageCoroutine(int instantEnemyNumber, int enemyCount, int waitTime)
     {
-        yield return new WaitForSeconds(1f); // 버그 방지용 임시 대기
-        UIManager.instance.UpdateStageText(Stagenumber-1);
-        
-        
         while (enemyCount > 0)
         {
-            GameObject enemy = Instantiate(instantEnemy, transform.position, transform.rotation);
-            yield return new WaitForSeconds(waitTime);
+            GameObject enemy = Instantiate(enemyPrefab[instantEnemyNumber], transform.position, transform.rotation);
             enemyCount--;
+            yield return new WaitForSeconds(waitTime);
         }
-        if (enemyCount == 0)
-        {
-            // Stagenumber -= 1;
-            EnemyCount += 1;
-        }
-        
+        stageNumber += 1;
+        yield return new WaitForSeconds(3f);
+        NextStage();
+    }
+
+    int[] SetStageData(int currentStage)
+    {
+        int[] stageData = new int[3];
+        stageData[0] = stageDictionary[currentStage][0];
+        stageData[1] = stageDictionary[currentStage][1];
+        stageData[2] = stageDictionary[currentStage][2];
+        return stageData;
+    }
+
+    void NextStage()
+    {
+        UIManager.instance.UpdateStageText(stageNumber);
+        int[] stageData = SetStageData(stageNumber);
+        StartCoroutine(StageCoroutine(stageData[0], stageData[1], stageData[2]));
     }
 }

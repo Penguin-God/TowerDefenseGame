508d65013dc334bf280e771741d96c2c8800594c
Penguin-God <gkswh4860@naver.com>
2021-04-11 16:19:40 +0900

    create SpearUnit

---
+++ Assets/1_Script/Unit/Unit_Spearman.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit_Spearman : TeamSoldier
+{
+    private Animator animator;
+    public BoxCollider spearCollider;
+    public GameObject trail;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        speed = 10f;
+        attackDelayTime = 2.5f;
+        attackRange = 3f;
+    }
+
+    public override void NormalAttack()
+    {
+        base.NormalAttack();
+        StartCoroutine(SwordAttack());
+    }
+
+    IEnumerator SwordAttack()
+    {
+        animator.SetTrigger("isAttack");
+        yield return new WaitForSeconds(0.1f);
+        trail.SetActive(true);
+        spearCollider.enabled = true;
+        yield return new WaitForSeconds(0.25f);
+        spearCollider.enabled = false;
+        trail.SetActive(false);
+    }
+}

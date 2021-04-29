c2f54c5f1a1aa222af99be34c439ff5787c5b4d9
jangjo123 <63893284+jangjo123@users.noreply.github.com>
2021-04-29 21:25:40 +0900

    유닛 관리 버튼 2

--- Assets/1_Script/UnitManageButton.cs
+++ Assets/1_Script/UnitManageButton.cs
@@ -5,10 +5,54 @@ using UnityEngine;
 public class UnitManageButton : MonoBehaviour
 {
     public GameObject UnitManagementButton;
+    public GameObject SwordmansButton;
+    public GameObject ArchersButton;
+    public GameObject SpearmansButton;
+    public GameObject MagesButton;
+    public GameObject ColorSwordmansButton;
+    public GameObject ColorArchersButton;
+    public GameObject ColorSpearmansButton;
+    public GameObject ColorMagesButton;
 
     public void FirstChilk()
     {
         UnitManagementButton.gameObject.SetActive(false);
+        SwordmansButton.gameObject.SetActive(true);
+        ArchersButton.gameObject.SetActive(true);
+        SpearmansButton.gameObject.SetActive(true);
+        MagesButton.gameObject.SetActive(true);
     }
-    
+
+    public void ManageButtonDown()
+    {
+        SwordmansButton.gameObject.SetActive(false);
+        ArchersButton.gameObject.SetActive(false);
+        SpearmansButton.gameObject.SetActive(false);
+        MagesButton.gameObject.SetActive(false);
+    }
+
+    public void ChlikSwordmanButton()
+    {
+        ManageButtonDown();
+        ColorSwordmansButton.gameObject.SetActive(true);
+    }
+
+    public void ChlikArcherButton()
+    {
+        ManageButtonDown();
+        ColorArchersButton.gameObject.SetActive(true);
+    }
+
+    public void ChlikSpearmanButton()
+    {
+        ManageButtonDown();
+        ColorSpearmansButton.gameObject.SetActive(true);
+    }
+
+    public void ChlikMageButton()
+    {
+        ManageButtonDown();
+        ColorMagesButton.gameObject.SetActive(true);
+    }
+
 }

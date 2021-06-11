b9de87c15f6b04881db6f68bb6d14e8084533c91
Penguin-God <gkswh4860@naver.com>
2021-06-11 15:59:35 +0900

    상점 질문 기능 틀 제작

--- Assets/1_Script/3_Event/Shop.cs
+++ Assets/1_Script/3_Event/Shop.cs
@@ -6,33 +6,20 @@ using System;
 
 public class Shop : MonoBehaviour
 {
-    // Action을 담는 리스트를 다르게 만들어 상점 기능 구현 ex) 유닛 소환은 void로 int 인자를 받고, 이벤트는 다른 타입의 인자를 받음
-    private int Productnumber;
-
-    private int SetRandomNumber()
-    {
-        Productnumber = UnityEngine.Random.Range(0, 2);
-        return Productnumber;
-    }
-
-    public Text LeftText;
-    public Text CenterText;
-    public Text RightText;
-
     public GameObject envetShop;
     public GameObject leftGoldGoods;
     public GameObject centerGoldGoods;
-    public GameObject rigthGoldGoods;
+    //public GameObject rigthGoldGoods;
     public GameObject foodGoods;
     // 변수명 반대로 하기
     public GameObject[] leftGoldStocks;
     public GameObject[] centerGoldStocks; 
-    public GameObject[] rigthGoldStocks; 
+    //public GameObject[] rigthGoldStocks; 
     public GameObject[] foodStocks;
 
     private GameObject current_LeftGoldGoods = null;
     private GameObject current_CenterGoldGoods = null;
-    private GameObject current_RigthGoldGoods = null;
+    //private GameObject current_RigthGoldGoods = null;
     private GameObject current_FoodGoldGoods = null;
 
     private void Awake() // 배열 선언
@@ -49,12 +36,6 @@ public class Shop : MonoBehaviour
             centerGoldStocks[i] = centerGoldGoods.transform.GetChild(i).gameObject;
         }
 
-        rigthGoldStocks = new GameObject[rigthGoldGoods.transform.childCount];
-        for (int i = 0; i < rigthGoldStocks.Length; i++)
-        {
-            rigthGoldStocks[i] = rigthGoldGoods.transform.GetChild(i).gameObject;
-        }
-
         foodStocks = new GameObject[foodGoods.transform.childCount];
         for (int i = 0; i < foodStocks.Length; i++)
         {
@@ -62,7 +43,6 @@ public class Shop : MonoBehaviour
         }
     }
 
-    // 고기 판매
     public void BuyFood(GameObject foodGoodsObject)
     {
         BuyGoods buyGoods = foodGoodsObject.GetComponent<BuyGoods>();
@@ -77,22 +57,18 @@ public class Shop : MonoBehaviour
         ExitShop();
     }
 
-    public void TestButton(Button button)
-    {
-        button.onClick.RemoveAllListeners();
-        button.onClick.AddListener(() => TestInt(100));
-        button.onClick.AddListener(() => TestString("Heelow "));
+    GameObject current_SelectGoods = null;
+    public GameObject buyFoodObject;
+    public Button foodBuyButton;
 
-    }
+    public GameObject buyUnitObject;
+    public Button unitBuyButton;
 
-    void TestInt(int aa)
+    public void Set_BuyFoodButton(GameObject foodGoodsObject)
     {
-        Debug.Log(aa);
-    }
-
-    void TestString(string aaaaa)
-    {
-        Debug.Log(aaaaa);
+        foodBuyButton.onClick.RemoveAllListeners();
+        buyFoodObject.SetActive(true);
+        foodBuyButton.onClick.AddListener(() => BuyFood(foodGoodsObject));
     }
 
     // 골드 판매
@@ -132,13 +108,15 @@ public class Shop : MonoBehaviour
 
         current_LeftGoldGoods.SetActive(false);
         current_CenterGoldGoods.SetActive(false);
-        current_RigthGoldGoods.SetActive(false);
+        //current_RigthGoldGoods.SetActive(false);
         current_FoodGoldGoods.SetActive(false);
 
         current_LeftGoldGoods = null;
         current_CenterGoldGoods = null;
-        current_RigthGoldGoods = null;
+        //current_RigthGoldGoods = null;
         current_FoodGoldGoods = null;
+
+        buyFoodObject.SetActive(false);
     }
 
     public void ShowShop()
@@ -157,7 +135,7 @@ public class Shop : MonoBehaviour
     {
         current_LeftGoldGoods = Show_RandomGoods(leftGoldGoods);
         current_CenterGoldGoods = Show_RandomGoods(centerGoldGoods);
-        current_RigthGoldGoods = Show_RandomGoods(rigthGoldGoods);
+        //current_RigthGoldGoods = Show_RandomGoods(rigthGoldGoods);
         current_FoodGoldGoods = Show_RandomGoods(foodGoods);
     }
 
@@ -175,155 +153,24 @@ public class Shop : MonoBehaviour
         return showGoods;
     }
 
-    public void UpdateLeftText() // 이미지로 바꾸면 좋을 듯
-    {
-        if(Productnumber == 0)
-        {
-            LeftText.text = "하얀 기사";
-        }
-
-        if(Productnumber == 1)
-        {
-            LeftText.text = "하얀 아처";
-        }
-
-        if (Productnumber == 2)
-        {
-            LeftText.text = "하얀 창병";
-        }
-    }
-
-    public void UpdateCenterText() // 이미지로 바꾸면 좋을 듯
-    {
-        if (Productnumber == 0)
-        {
-            CenterText.text = "고기 1개";
-        }
-
-        if (Productnumber == 1)
-        {
-            CenterText.text = "고기 2개";
-        }
-
-        if (Productnumber == 2)
-        {
-            CenterText.text = "고기 3개";
-        }
-    }
-
-    public void UpdateRightText() // 이미지로 바꾸면 좋을 듯
-    {
-        if (Productnumber == 0)
-        {
-
-            RightText.text = "데미지 증가";
-        }
-        if (Productnumber == 1)
-        { 
-           RightText.text = "보스 데미지 증가";
-        }
-
-        if (Productnumber == 2)
-        {
-            RightText.text = "다른 효과";
-        }
-        
-    }
-
-    //public void BuyGoods(int price)
+    //public void UpdateRightText() // 이미지로 바꾸면 좋을 듯
     //{
-    //    if (GameManager.instance.Gold < price) return;
+    //    if (Productnumber == 0)
+    //    {
 
-    //    GameManager.instance.Gold -= price;
-    //    UIManager.instance.UpdateGoldText(GameManager.instance.Gold);
+    //        RightText.text = "데미지 증가";
+    //    }
+    //    if (Productnumber == 1)
+    //    { 
+    //       RightText.text = "보스 데미지 증가";
+    //    }
+
+    //    if (Productnumber == 2)
+    //    {
+    //        RightText.text = "다른 효과";
+    //    }
+        
     //}
-
-    public void ClickLeftGoods()
-    {
-        if (Productnumber == 0 && GameManager.instance.Gold >= 5)
-        {
-            createDefenser.CreateSoldier(7, 0);
-
-            GameManager.instance.Gold -= 5;
-            UIManager.instance.UpdateGoldText(GameManager.instance.Gold);
-
-            envetShop.SetActive(false);
-        }
-
-        if (Productnumber == 1 && GameManager.instance.Gold >= 10)
-        {
-            createDefenser.CreateSoldier(7, 1);
-
-            GameManager.instance.Gold -= 10;
-            UIManager.instance.UpdateGoldText(GameManager.instance.Gold);
-
-            envetShop.SetActive(false);
-        }
-
-        if (Productnumber == 2 && GameManager.instance.Gold >= 15)
-        {
-            createDefenser.CreateSoldier(7, 2);
-
-            GameManager.instance.Gold -= 15;
-            UIManager.instance.UpdateGoldText(GameManager.instance.Gold);
-
-            envetShop.SetActive(false);
-        }
-    }
-
-    public void ClickCenterGoods()
-    {
-        if (Productnumber == 0 && GameManager.instance.Gold >= 5)
-        {
-            GameManager.instance.Food += 1;
-            UIManager.instance.UpdateFoodText(GameManager.instance.Food);
-
-            GameManager.instance.Gold -= 5;
-            UIManager.instance.UpdateGoldText(GameManager.instance.Gold);
-
-            envetShop.SetActive(false);
-        }
-
-        if (Productnumber == 1 && GameManager.instance.Gold >= 10)
-        {
-            GameManager.instance.Food += 2;
-            UIManager.instance.UpdateFoodText(GameManager.instance.Food);
-
-            GameManager.instance.Gold -= 10;
-            UIManager.instance.UpdateGoldText(GameManager.instance.Gold);
-
-            envetShop.SetActive(false);
-        }
-
-        if (Productnumber == 2 && GameManager.instance.Gold >= 15)
-        {
-            GameManager.instance.Food += 3;
-            UIManager.instance.UpdateFoodText(GameManager.instance.Food);
-
-            GameManager.instance.Gold -= 15;
-            UIManager.instance.UpdateGoldText(GameManager.instance.Gold);
-
-            envetShop.SetActive(false);
-        }
-    }
-
-    public void ClickRightGoods()
-    {
-        if (Productnumber == 0)
-        {
-            envetShop.SetActive(false);
-        }
-
-        if (Productnumber == 1)
-        {
-            envetShop.SetActive(false);
-        }
-
-        if (Productnumber == 2)
-        {
-            envetShop.SetActive(false);
-        }
-    }
 
     //public void EnterShopWlord()
     //{

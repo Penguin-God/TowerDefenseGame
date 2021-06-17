8831d9eb2f4e498df98ab58c753050c1ca1ef96e
jangjo123 <63893284+jangjo123@users.noreply.github.com>
2021-06-17 21:36:59 +0900

    광고 달기

---
+++ Assets/Client/AdManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class AdManager : MonoBehaviour
+{
+    public void ShowAD()
+    {
+        Debug.Log("클릭 됨");
+        if (Advertisement.IsReady())
+        {
+            Advertisement.Show();
+        }
+    }
+
+    private void Awake()
+    {
+        Advertisement.Initialize("4174571", true);
+    }
+
+    // 4174570 Apple
+    // 4174571 Android
+}

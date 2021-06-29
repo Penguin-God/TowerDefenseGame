dc0d00ff0fab5ef9ad2bfa420c83a20eaf137411
jangjo123 <63893284+jangjo123@users.noreply.github.com>
2021-06-29 21:42:06 +0900

    광고 추가 키스토어

--- Assets/1_Script/GameManager.cs
+++ Assets/1_Script/GameManager.cs
@@ -177,8 +177,16 @@ public class GameManager : MonoBehaviour
     int FasterCount = 0;
     public Text FasterText;
     public float gameTimeSpeed;
+
+    int FasterAdCount = 0;
+    public AdManager adManager;
     public void ClickFasterButton() // 클릭 할때 마다 게임 속도 증가
     {
+        if(FasterAdCount == 0)
+        {
+            adManager.ShowRewardAd();
+            FasterAdCount += 1;
+        }
         unitManageButton.UnitManageAudio.Play();
         if (FasterCount == 0)
         {

64db7de84a5cc4f634f915b3fe37515e84a2bc39
Penguin-God <gkswh4860@naver.com>
2021-08-05 21:58:04 +0900

    ui addEvent

---
+++ Assets/1_Script/1_Unit/AddOnClickEvent.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AddOnClickEvent : MonoBehaviour
+{
+    [SerializeField] Transform obj_UnitManageUI;
+    GameObject currentShowUI = null;
+    private void Start()
+    {
+        for(int i = 0; i < transform.childCount; i++)
+        {
+            Button button = transform.GetChild(i).GetComponent<Button>();
+            button.onClick.AddListener(() => obj_UnitManageUI.transform.GetChild(i).gameObject.SetActive(true));
+            button.onClick.AddListener(() => ShowUnitUI() );
+            button.onClick.AddListener(() => currentShowUI = obj_UnitManageUI.GetChild(i).gameObject);
+        }
+    }
+
+    void ShowUnitUI()
+    {
+        if (currentShowUI != null) currentShowUI.SetActive(false);
+    }
+}

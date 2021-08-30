a93f5aea77ee7de3781f548d044fbd118eb79a48
Penguin-God <gkswh4860@naver.com>
2021-08-30 20:57:21 +0900

    유닛 패시브 코드 닦는 중

---
+++ Assets/1_Script/1_Unit/UnitPassives/UnitPassive.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitPassive : MonoBehaviour
+{
+    TeamSoldier teamSoldier;
+
+    private void Start()
+    {
+        teamSoldier = GetComponent<TeamSoldier>();
+        if(teamSoldier == null)
+        {
+            Debug.LogWarning("이상한 곳에 스크립트 배정함");
+        }
+    }
+}

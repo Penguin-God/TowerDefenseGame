3b972ed4f41db4e92c6c2f62da8f6d0c3bbb8820
jangjo123 <63893284+jangjo123@users.noreply.github.com>
2022-01-03 23:35:27 +0900

    pooling

---
+++ Assets/1_Script/CombineSoldierPooling.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombineSoldierPooling : MonoBehaviour
+{
+    public static ObjectPool Instance;
+    [SerializeField] private GameObject poolingObjectPrefab;
+    Queue<Unit_Swordman> poolingObjectQueue = new Queue<Unit_Swordman>();
+    private void Awake()
+    {
+        Instance = this; Initialize(10);
+    }
+    private void Initialize(int initCount)
+    {
+        for (int i = 0; i < initCount; i++)
+        {
+            poolingObjectQueue.Enqueue(CreateNewObject()); 
+        } 
+    }
+    private Unit_Swordman CreateNewObject() 
+    {
+        var newObj = Instantiate(poolingObjectPrefab).GetComponent<Unit_Swordman>(); newObj.gameObject.SetActive(false); newObj.transform.SetParent(transform); return newObj;
+    }
+    public static Unit_Swordman GetObject() {
+        if (Instance.poolingObjectQueue.Count > 0)
+        { var obj = Instance.poolingObjectQueue.Dequeue();
+            obj.transform.SetParent(null); obj.gameObject.SetActive(true);
+            return obj;
+        }
+        else 
+        {
+            var newObj = Instance.CreateNewObject();
+            newObj.gameObject.SetActive(true);
+            newObj.transform.SetParent(null);
+            return newObj; 
+        } 
+    }
+    public static void ReturnObject(Unit_Swordman obj) 
+    {
+        obj.gameObject.SetActive(false);
+        obj.transform.SetParent(Instance.transform);
+        Instance.poolingObjectQueue.Enqueue(obj);
+    }
+
+}

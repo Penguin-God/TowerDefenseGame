53d1c5c8156a697de2976abcd934c783a0a26da3
Penguin-God <gkswh4860@naver.com>
2022-02-08 17:50:50 +0900

    멀티 개발 도우미 스크립트 생성

---
+++ Assets/0_Multi/MultiDevelopHelper.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class MultiDevelopHelper : MonoBehaviourPunCallbacks
+{
+    [SerializeField] Text guideText = null;
+
+    [ContextMenu("방 생성 및 입장")]
+
+    void EditorConnect()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        guideText.text = $"서버 연결 됨.";
+        Connect();
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        guideText.text = "연결 실패 재접속 중...";
+
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public void Connect() 
+    {
+        if (PhotonNetwork.IsConnected)
+        {
+            guideText.text = "방에 들어가는 중...";
+            PhotonNetwork.JoinRandomRoom();
+        }
+        else
+        {
+            guideText.text = "연결 실패 재접속 중...";
+            PhotonNetwork.ConnectUsingSettings();
+        }
+    }
+
+    public override void OnJoinedRoom()
+    {
+        guideText.text = "방 참가 성공";
+
+        PhotonNetwork.LoadLevel("박준 멀티");
+    }
+
+    // 방 접속 실패 시 방 생성
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        guideText.text = "빈 방 없음, 새로운 방 생성 중...";
+
+        PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = 2 });
+    }
+}

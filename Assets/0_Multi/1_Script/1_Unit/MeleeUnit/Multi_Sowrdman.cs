32e49561d7b825296a5b6f089cc3b75863559902
Penguin-God <gkswh4860@naver.com>
2022-02-15 18:16:42 +0900

    폴더 정리

---
+++ Assets/0_Multi/1_Script/1_Unit/MeleeUnit/Multi_Sowrdman.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Multi_Sowrdman : Multi_MeleeUnit
+{
+    [Header("기사 변수")]
+    [SerializeField] GameObject trail;
+
+    [PunRPC]
+    public override void NormalAttack()
+    {
+        StartCoroutine("SwordAttack");
+    }
+
+    IEnumerator SwordAttack()
+    {
+        base.StartAttack();
+
+        animator.SetTrigger("isSword");
+        yield return new WaitForSeconds(0.8f);
+        trail.SetActive(true);
+        yield return new WaitForSeconds(0.3f);
+        HitMeeleAttack();
+        trail.SetActive(false);
+
+        base.NormalAttack();
+    }
+}

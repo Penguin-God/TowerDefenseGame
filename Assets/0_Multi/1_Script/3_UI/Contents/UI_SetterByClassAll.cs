e99576eb9c5dde0c6b88bce0888d983a70b5d013
Penguin-God <gkswh4860@naver.com>
2022-05-27 18:54:14 +0900

    유닛 트래커 모드 전환 끝

--- Assets/0_Multi/1_Script/3_UI/Contents/UI_SetterByClassAll.cs
+++ Assets/0_Multi/1_Script/3_UI/Contents/UI_SetterByClassAll.cs
@@ -4,5 +4,9 @@ using UnityEngine;
 
 public class UI_SetterByClassAll : UI_UnitTrackerSetterBase
 {
-
+    public override void SettingUnitTrackers(UI_UnitTrackerData data)
+    {
+        base.SettingUnitTrackers(data);
+        // TODO : 모든 유닛들 클래스별로 모아서 카운팅하는 기능 구현하기
+    }
 }

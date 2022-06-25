3e015611b82961558177a26b01d2c23a58c9335b
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2022-06-25 16:30:33 +0900

    죽은 코드 제거하기, region 추가

--- Assets/0_Multi/1_Script/5_Utility/Utility/Parser/CsvParsers.cs
+++ Assets/0_Multi/1_Script/5_Utility/Utility/Parser/CsvParsers.cs
@@ -30,7 +30,6 @@ public abstract class CsvParsers
 {
     public static CsvParser GetParser(FieldInfo info)
     {
-        // Debug.Log(IsEnumerable(info.FieldType.Name));
         if (IsEnumerable(info.FieldType.Name))
             return new EnumerableTypeParser(info);
         else
@@ -53,6 +52,28 @@ public abstract class CsvParsers
         }
         return null;
     }
+}
+
+
+class PrimitiveTypeParser : CsvParser
+{
+    public object GetParserValue(string typeName, string value)
+    {
+        typeName = typeName.Replace("System.", "");
+        switch (typeName)
+        {
+            case nameof(Int32): return new CsvIntParser().GetParserValue(value);
+            case nameof(Single): return new CsvFloatParser().GetParserValue(value);
+            case nameof(Boolean): return new CsvBooleanParser().GetParserValue(value);
+            case nameof(String): return new CsvStringParser().GetParserValue(value);
+            case nameof(UnitFlags): return new CsvUnitFlagsParser().GetParserValue(value);
+            default: Debug.LogError("Csv 파싱 타입을 찾지 못함"); break;
+        }
+        return null;
+    }
+
+    public void SetValue(object obj, FieldInfo info, string[] value)
+        => info.SetValue(obj, GetParserValue(info.FieldType.Name, value[0]));
 }
 
 class EnumerableTypeParser : CsvParser
@@ -88,79 +109,13 @@ class EnumerableTypeParser : CsvParser
     {
         values = values.Where(x => !string.IsNullOrEmpty(x)).ToArray();
         if (_type == EnumerableType.Array)
-        {
             new CsvArrayParser().SetValue(obj, info, values, _elementTypeName);
-            //ConstructorInfo constructor = List<object>.FieldType.GetConstructors()[2];
-            //info.SetValue(obj, CsvParsers.GetPrimitiveParser(_elementTypeName).GetParserEnumerable(values).);
-
-            //switch (_elementTypeName)
-            //{
-            //    case nameof(Int32): parserValue = valuesList.Select(x => (int)x).ToArray(); break;
-            //    case nameof(Single): parserValue = valuesList.Select(x => (float)x).ToArray(); break;
-            //    case nameof(Boolean): parserValue = valuesList.Select(x => (bool)x).ToArray(); break;
-            //    case nameof(String): parserValue = valuesList.Select(x => (string)x).ToArray(); break;
-            //    case nameof(UnitFlags): parserValue = valuesList.Select(x => (UnitFlags)x).ToArray(); break;
-            //    default: Debug.LogError("Csv 파싱 타입을 찾지 못함"); break;
-            //}
-        }
         else if (_type == EnumerableType.List)
-        {
-            
-            //switch (_elementTypeName)
-            //{
-            //    case nameof(Int32): parserValue = valuesList.Select(x => (int)x).ToList(); break;
-            //    case nameof(Single): parserValue = valuesList.Select(x => (float)x).ToList(); break;
-            //    case nameof(Boolean): parserValue = valuesList.Select(x => (bool)x).ToList(); break;
-            //    case nameof(String): parserValue = valuesList.Select(x => (string)x).ToList(); break;
-            //    case nameof(UnitFlags): parserValue = valuesList.Select(x => (UnitFlags)x).ToList(); break;
-            //    default: Debug.LogError("Csv 파싱 타입을 찾지 못함"); break;
-            //}
             new CsvListParser().SetValue(obj, info, values, _elementTypeName);
-        }
     }
 }
 
-public class CsvListParser
-{
-    public void SetValue(object obj, FieldInfo info, string[] values, string typeName)
-    {
-        ConstructorInfo constructor = info.FieldType.GetConstructors()[2];
-        info.SetValue(obj, constructor.Invoke(new object[] { CsvParsers.GetPrimitiveParser(typeName).GetParserEnumerable(values) }));
-    }
-}
-
-public class CsvArrayParser
-{
-    public void SetValue(object obj, FieldInfo info, string[] values, string typeName)
-    {
-        Array array = Array.CreateInstance(CsvParsers.GetPrimitiveParser(typeName).GetParserType(), values.Length);
-        for (int i = 0; i < array.Length; i++)
-            array.SetValue(CsvParsers.GetPrimitiveParser(typeName).GetParserValue(values[i]), i);
-        info.SetValue(obj, array);
-    }
-}
-
-
-class PrimitiveTypeParser : CsvParser
-{
-    public object GetParserValue(string typeName, string value)
-    {
-        typeName = typeName.Replace("System.", "");
-        switch (typeName)
-        {
-            case nameof(Int32): return new CsvIntParser().GetParserValue(value);
-            case nameof(Single): return new CsvFloatParser().GetParserValue(value);
-            case nameof(Boolean): return new CsvBooleanParser().GetParserValue(value);
-            case nameof(String): return new CsvStringParser().GetParserValue(value);
-            case nameof(UnitFlags): return new CsvUnitFlagsParser().GetParserValue(value);
-            default: Debug.LogError("Csv 파싱 타입을 찾지 못함"); break;
-        }
-        return null;
-    }
-
-    public void SetValue(object obj, FieldInfo info, string[] value)
-        => info.SetValue(obj, GetParserValue(info.FieldType.Name, value[0]));
-}
+#region 기본형 파싱
 
 class CsvIntParser : CsvPrimitiveTypeParser
 {
@@ -210,7 +165,7 @@ class CsvUnitFlagsParser : CsvPrimitiveTypeParser
 {
     public object GetParserValue(string value)
     {
-        if(value.Split('&').Length == 2)
+        if (value.Split('&').Length == 2)
             return new UnitFlags(value.Split('&')[0], value.Split('&')[1]);
         else
             return Multi_Managers.Data.UnitNameDataByUnitKoreaName[value].UnitFlags;
@@ -220,26 +175,27 @@ class CsvUnitFlagsParser : CsvPrimitiveTypeParser
     public Type GetParserType() => typeof(UnitFlags);
 }
 
-class CsvPairParser : CsvPrimitiveTypeParser
+#endregion 기본형 파싱 End
+
+
+#region 열거형 파싱
+public class CsvListParser
 {
-    public IEnumerable GetParserEnumerable(string[] values)
+    public void SetValue(object obj, FieldInfo info, string[] values, string typeName)
     {
-        throw new NotImplementedException();
-    }
-
-    public Type GetParserType()
-    {
-        throw new NotImplementedException();
-    }
-
-    public object GetParserValue(string value)
-    {
-        return null;
-    }
-
-    public object GetParserValue(string value, Type type)
-    {
-        
-        return null;
+        ConstructorInfo constructor = info.FieldType.GetConstructors()[2];
+        info.SetValue(obj, constructor.Invoke(new object[] { CsvParsers.GetPrimitiveParser(typeName).GetParserEnumerable(values) }));
     }
 }
+
+public class CsvArrayParser
+{
+    public void SetValue(object obj, FieldInfo info, string[] values, string typeName)
+    {
+        Array array = Array.CreateInstance(CsvParsers.GetPrimitiveParser(typeName).GetParserType(), values.Length);
+        for (int i = 0; i < array.Length; i++)
+            array.SetValue(CsvParsers.GetPrimitiveParser(typeName).GetParserValue(values[i]), i);
+        info.SetValue(obj, array);
+    }
+}
+#endregion 열거형 파싱 End

5ad4d844d9797da82d44a2b0cf215f41eefbabc8
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2022-07-21 15:24:49 +0900

    RaiseEvent 방식으로 RPCAction 구현

--- Assets/0_Multi/1_Script/RPCAction.cs
+++ Assets/0_Multi/1_Script/RPCAction.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using Photon.Realtime;
+using ExitGames.Client.Photon;
 using System;
 
 // 마스터한테 요청해야 하지만 개별로 적용되어야 하는 이벤트들
@@ -22,6 +24,50 @@ public class RPCAction
     {
         if (Multi_Data.instance.CheckIdSame(id) == false) return;
         action?.Invoke();
+    }
+}
+
+public class RPCAction<T>
+{
+    static byte currentEventID = 0;
+    byte _eventId;
+
+    event Action<T> OnEvent = null;
+
+    public RPCAction()
+    {
+        _eventId = currentEventID++;
+        PhotonNetwork.NetworkingClient.EventReceived += RecevieEvent;
+    }
+
+    void RecevieEvent(EventData data)
+    {
+        if (data.Code != _eventId) return;
+        T value = (T)((object[])data.CustomData)[0];
+        OnEvent?.Invoke(value);
+    }
+
+    public void RaiseEvent(int id, T value) // 내가 맞으면 실행하고 아니면 전달
+    {
+        if (Multi_Data.instance.CheckIdSame(id))
+        {
+            PhotonNetwork.RaiseEvent(_eventId, new object[] { value }, new RaiseEventOptions(){ Receivers = ReceiverGroup.Others }, SendOptions.SendUnreliable);
+            OnEvent?.Invoke(value);
+            return;
+        }
+        //PhotonNetwork.RaiseEvent(_eventId, new object[] { value }, new RaiseEventOptions() { Receivers = ReceiverGroup.Others }, SendOptions.SendUnreliable);
+    }
+
+    public static RPCAction<T> operator +(RPCAction<T> me, Action<T> action)
+    {
+        me.OnEvent += action;
+        return me;
+    }
+
+    public static RPCAction<T> operator -(RPCAction<T> me, Action<T> action)
+    {
+        me.OnEvent -= action;
+        return me;
     }
 }
 
---
+++ Assets/0_Multi/1_Script/TestRaise.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class TestRaise : MonoBehaviourPun
+{
+    RPCAction<int> test = new RPCAction<int>();
+    [SerializeField] int value;
+    void Awake()
+    {
+        test += Multi_GameManager.instance.AddGold;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            Debug.Log("click");
+            test.RaiseEvent(Multi_Data.instance.Id, value);
+        }
+    }
+}

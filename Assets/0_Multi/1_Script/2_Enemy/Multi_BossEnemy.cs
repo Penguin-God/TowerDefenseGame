9a44b171708ea32cdc5ae6f04c288f310f116d83
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2022-07-23 18:34:08 +0900

    보스 잡을시 보상 구현

--- Assets/0_Multi/1_Script/2_Enemy/Multi_BossEnemy.cs
+++ Assets/0_Multi/1_Script/2_Enemy/Multi_BossEnemy.cs
@@ -9,10 +9,11 @@ public class Multi_BossEnemy : Multi_NormalEnemy
     [SerializeField] int _level;
     public int Level => _level;
 
+    public BossData BossData { get; private set; }
     public void Spawn(int level)
     {
         _level = level;
-        BossData data = Multi_Managers.Data.BossDataByLevel[_level];
-        SetStatus_RPC(data.Hp, data.Speed, false);
+        BossData = Multi_Managers.Data.BossDataByLevel[_level];
+        SetStatus_RPC(BossData.Hp, BossData.Speed, false);
     }
 }

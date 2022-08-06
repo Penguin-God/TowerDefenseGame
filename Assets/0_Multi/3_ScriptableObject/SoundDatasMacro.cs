aebf682deb3180695c5cfdb46af4351853bb7578
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2022-08-06 13:23:53 +0900

    사운드 매크로 제작

---
+++ Assets/0_Multi/3_ScriptableObject/SoundDatasMacro.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Text;
+
+[CreateAssetMenu(fileName = "SoundDatasMacro", menuName = "Macro/Sounds")]
+public class SoundDatasMacro : ScriptableObject
+{
+    [SerializeField, TextArea] string _enumTexts;
+    const string basePath = "C:/Users/parkj/Desktop/Current Project/1.ColorRandomDefense/Assets/0_Multi/Resources/Sounds/";
+    const string filePath = "C:/Users/parkj/Desktop/Current Project/1.ColorRandomDefense/Assets/0_Multi/Resources/Data/SoundData.csv";
+
+    [ContextMenu("Save Csv File")]
+    void SaveCsv()
+    {
+        StringBuilder stringBuilder = new StringBuilder();
+        foreach (string path in Directory.GetFiles(basePath, "*.wav", SearchOption.AllDirectories))
+        {
+            string value = path.Replace(basePath, "").Replace(".wav", "").Replace("\\", "/");
+            stringBuilder.Append(value.Split('/')[value.Split('/').Length - 1]);
+            stringBuilder.Append(",");
+            stringBuilder.Append(value);
+            stringBuilder.Append('\n');
+        }
+        CsvUtility.SaveCsv(stringBuilder.ToString(), filePath);
+    }
+
+    [ContextMenu("Set Enum Text")]
+    void SetEnumText()
+    {
+        StringBuilder stringBuilder = new StringBuilder();
+        foreach (string path in Directory.GetFiles(basePath, "*.wav", SearchOption.AllDirectories))
+        {
+            string value = path.Replace(basePath, "").Replace(".wav", "");
+            stringBuilder.Append(value.Split('\\')[value.Split('\\').Length - 1]);
+            stringBuilder.Append(',');
+            stringBuilder.Append('\n');
+        }
+        _enumTexts = stringBuilder.ToString();
+    }
+}

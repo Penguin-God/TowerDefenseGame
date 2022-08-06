c7dca80a0d27780ddf67e9cc5a71da256e770f58
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2022-08-06 12:13:11 +0900

    사운드 데이터 매크로 기능 구현

--- Assets/0_Multi/1_Script/5_Utility/Utility/TestUtility.cs
+++ Assets/0_Multi/1_Script/5_Utility/Utility/TestUtility.cs
@@ -1,16 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System;
 
 public class TestUtility : MonoBehaviour
 {
     [SerializeField] UnitFlags flag;
+
+    [SerializeField, TextArea] string texts;
+
+    const string basePath = "C:/Users/parkj/Desktop/Current Project/1.ColorRandomDefense/Assets/0_Multi/Resources/Sounds/";
     [ContextMenu("Test")]
     void Test()
     {
-        Multi_Managers.Sound.PlayEffect(EffectSoundType.SwordmanAttack);
+        StringBuilder stringBuilder = new StringBuilder();
+        foreach (string path in Directory.GetFiles(basePath, "*.wav", SearchOption.AllDirectories))
+        {
+            string value = path.Replace(basePath, "").Replace(".wav", "");
+            stringBuilder.Append(value.Split('\\')[value.Split('\\').Length - 1]);
+            stringBuilder.Append(",");
+            stringBuilder.Append(value);
+            stringBuilder.Append('\n');
+        }
+        texts = stringBuilder.ToString();
+        // Multi_Managers.Sound.PlayEffect(EffectSoundType.SwordmanAttack);
     }
 
     void Update()

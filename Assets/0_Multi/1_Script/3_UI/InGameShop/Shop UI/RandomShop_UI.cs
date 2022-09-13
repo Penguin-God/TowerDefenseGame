9a49daa08f31490566e23ed1e53cbfcb3def30ab
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2022-09-13 18:54:37 +0900

    특정 등급 없을 경우 예외처리

--- Assets/0_Multi/1_Script/3_UI/InGameShop/Shop UI/RandomShop_UI.cs
+++ Assets/0_Multi/1_Script/3_UI/InGameShop/Shop UI/RandomShop_UI.cs
@@ -2,17 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Linq;
 
 class GoodsManager
 {
     const int goodsTypeCount = 3;
     const int maxGrade = 3;
     List<UI_RandomShopGoodsData>[,] _goodsDatas;
+    Dictionary<GoodsLocation, List<UI_RandomShopGoodsData>[]> _goodsData;
     public GoodsManager()
     {
         Setup();
         foreach (var data in Multi_Managers.Data.RandomShopDatas)
             _goodsDatas[(int)data.GoodsLocation, data.Grade].Add(data);
+
+        _goodsData = GeneratedGoodsData();
+    }
+
+    Dictionary<GoodsLocation, List<UI_RandomShopGoodsData>[]> GeneratedGoodsData()
+    {
+        var goodsData = new Dictionary<GoodsLocation, List<UI_RandomShopGoodsData>[]>();
+        foreach (var data in Multi_Managers.Data.RandomShopDatas)
+        {
+            if (goodsData.ContainsKey(data.GoodsLocation) == false)
+            {
+                goodsData.Add(data.GoodsLocation, new List<UI_RandomShopGoodsData>[maxGrade]);
+                for (int i = 0; i < goodsData[data.GoodsLocation].Length; i++)
+                    goodsData[data.GoodsLocation][i] = new List<UI_RandomShopGoodsData>();
+            }
+                
+            goodsData[data.GoodsLocation][data.Grade].Add(data);
+        }
+
+        return goodsData;
     }
 
     void Setup()
@@ -34,19 +56,48 @@ class GoodsManager
 
         for (int i = 0; i < result.Length; i++)
         {
-            List<UI_RandomShopGoodsData> datas = _goodsDatas[i, GetGrade(new int[] { 33, 33, 34 })];
+            List<UI_RandomShopGoodsData> datas = _goodsDatas[i, GetGrade((GoodsLocation)i, new int[] { 33, 33, 34 })];
             result[i] = datas[Random.Range(0, datas.Count)];
         }
 
         return result;
     }
 
+    int GetGrade(GoodsLocation location, int[] weigths)
+    {
+        var gradeByWeith = GeneratedGradeByWeigth(location, weigths);
+        int randomNumber = Random.Range(0, gradeByWeith.Values.Sum());
+
+        foreach (var item in gradeByWeith)
+        {
+            if (item.Value >= randomNumber) return item.Key;
+            else randomNumber -= item.Value;
+        }
+
+        Debug.LogError("확률 잘못 설정함");
+        return 0;
+    }
+
+    Dictionary<int, int> GeneratedGradeByWeigth(GoodsLocation location, int[] weigths)
+    {
+        var result = new Dictionary<int, int>();
+        for (int i = 0; i < maxGrade; i++)
+        {
+            if (ContainsGoods(location, i))
+                result.Add(i, weigths[i]);
+        }
+        return result;
+    }
+
+    bool ContainsGoods(GoodsLocation location, int grade) => _goodsData[location][grade].Count > 0;
+
     int GetGrade(int[] weigths)
     {
         int totalWeigh = 100;
+
         int randomNumber = Random.Range(0, totalWeigh);
 
-        for (int i = 0; i < maxGrade; i++) // 레벨 가중치에 따라 상품 등급 정함
+        for (int i = 0; i < maxGrade; i++) // 확률 설정에 따라 가중치 적용 가능
         {
             if (weigths[i] >= randomNumber) return i;
             else randomNumber -= weigths[i];
@@ -82,8 +133,8 @@ public class RandomShop_UI : Multi_UI_Popup
 
     void BindGoods()
     {
-        UI_RandomShopGoodsData[] datas = goodsManager.GetRandomGoods();
         currentGoodsList.Clear();
+        UI_RandomShopGoodsData[] datas = goodsManager.GetRandomGoods();
         for (int i = 0; i < goodsParent.childCount; i++)
         {
             Goods_UI goods = goodsParent.GetChild(i).GetComponent<Goods_UI>();
@@ -110,7 +161,6 @@ public class RandomShop_UI : Multi_UI_Popup
     // 리셋 버튼에서 사용하는 함수
     public void ShopReset()
     {
-
         panel.Setup(BindGoods, 10, "Gold", "10골드를 지불하여 상점을 돌리시겠습니까?");
         Multi_Managers.Sound.PlayEffect(EffectSoundType.ShopGoodsClick);
     }

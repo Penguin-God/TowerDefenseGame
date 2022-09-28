bf3722bddf34ca2f58f8ec2e5c074e100fc47697
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2022-09-28 12:52:01 +0900

    적군의 성 이동 버튼 텍스트 디버그

--- Assets/0_Multi/1_Script/3_UI/Contents/Button/LookTowerButton.cs
+++ Assets/0_Multi/1_Script/3_UI/Contents/Button/LookTowerButton.cs
@@ -5,9 +5,11 @@ using UnityEngine.UI;
 
 public class LookTowerButton : Multi_UI_Scene
 {
+    Text _text;
     void Awake()
     {
         GetComponentInChildren<Button>().onClick.AddListener(CameraPositionChanged);
+        _text = GetComponentInChildren<Text>();
     }
 
     void CameraPositionChanged()
@@ -15,8 +17,14 @@ public class LookTowerButton : Multi_UI_Scene
         Multi_Managers.UI.CloseAllPopupUI();
         Multi_Managers.Sound.PlayEffect(EffectSoundType.PopSound);
         if (Multi_Managers.Camera.IsLookEnemyTower)
+        {
             Multi_Managers.Camera.LookWorld();
+            _text.text = "적군의 성으로";
+        }
         else
+        {
             Multi_Managers.Camera.LookEnemyTower();
+            _text.text = "월드로";
+        }
     }
 }

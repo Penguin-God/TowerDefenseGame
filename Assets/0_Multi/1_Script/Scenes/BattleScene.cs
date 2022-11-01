cbb7309a0fa430cc46baaf07188ecf4ed7c82b22
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2022-11-01 18:18:04 +0900

    죽은 코드 제거하기

--- Assets/0_Multi/1_Script/Scenes/BattleScene.cs
+++ Assets/0_Multi/1_Script/Scenes/BattleScene.cs
@@ -53,7 +53,6 @@ public class BattleScene : BaseScene
         Multi_Managers.UI.ShowPopupUI<CombineResultText>("CombineResultText");
         Multi_Managers.UI.ShowPopupUI<WarningText>();
         Multi_Managers.UI.ShowPopupUI<RandomShop_UI>("InGameShop/Random Shop");
-        Multi_Managers.UI.ShowPopupUI<EnemyPlayerInfoWindow>("EnemyPlayerInfoWindow");
 
         Multi_Managers.UI.ShowSceneUI<Status_UI>();
         Multi_Managers.UI.ShowSceneUI<LookWorldChangedButton>("LookWorldChangedButton");
@@ -61,7 +60,6 @@ public class BattleScene : BaseScene
         Multi_Managers.UI.ShowSceneUI<Multi_UI_Paint>("Paint");
         Multi_Managers.UI.ShowSceneUI<CreateDefenserButton>("Create Defenser Button");
         Multi_Managers.UI.ShowSceneUI<LookTowerButton>("Story Wolrd Enter Button");
-        //Multi_Managers.UI.ShowSceneUI<EnemyPlayerStatusShowButton>("EnemyPlayerStatusShowButton");
     }
 
     public override void Clear()

fbca80167924358587341288df887b8259a8465c
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2022-11-17 18:51:58 +0900

    UI_Barrier 제작

---
+++ Assets/0_Multi/1_Script/3_UI/UI_Barrier.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Barrier : Multi_UI_Base
+{
+    protected override void Init()
+    {
+        base.Init();
+        BindEvnet(gameObject, (data) => Multi_Managers.UI.ClosePopupUI());
+    }
+}

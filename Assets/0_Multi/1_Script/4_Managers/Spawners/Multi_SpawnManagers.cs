8e4ab4ddaa6a65294ea889ccb85e8a38dd721fb0
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2022-12-01 19:38:17 +0900

    죽은 코드 제거

--- Assets/0_Multi/1_Script/4_Managers/Spawners/Multi_SpawnManagers.cs
+++ Assets/0_Multi/1_Script/4_Managers/Spawners/Multi_SpawnManagers.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
-using System;
 
 public class Multi_SpawnManagers : MonoBehaviourPun
 {
@@ -25,14 +24,12 @@ public class Multi_SpawnManagers : MonoBehaviourPun
     Multi_TowerEnemySpawner _towerEnemy;
     Multi_NormalUnitSpawner _normalUnit;
     Multi_WeaponSpawner _weapon;
-    EffectSpawner _effect;
 
     public static Multi_NormalEnemySpawner NormalEnemy => Instance._normalEnemy;
     public static Multi_BossEnemySpawner BossEnemy => Instance._bossEnemy;
     public static Multi_TowerEnemySpawner TowerEnemy => Instance._towerEnemy;
     public static Multi_NormalUnitSpawner NormalUnit => Instance._normalUnit;
     public static Multi_WeaponSpawner Weapon => Instance._weapon;
-    public static EffectSpawner Effect => Instance._effect;
 
     // TODO : 딱 봐도 지옥인데 이거 좀 개선하기
     void Awake()
@@ -42,7 +39,6 @@ public class Multi_SpawnManagers : MonoBehaviourPun
         _towerEnemy = GetOrAddChildComponent<Multi_TowerEnemySpawner>();
         _normalUnit = GetOrAddChildComponent<Multi_NormalUnitSpawner>();
         _weapon = GetOrAddChildComponent<Multi_WeaponSpawner>();
-        _effect = GetOrAddChildComponent<EffectSpawner>();
     }
 
     public void Init()
@@ -52,7 +48,6 @@ public class Multi_SpawnManagers : MonoBehaviourPun
         _towerEnemy = GetOrAddChildComponent<Multi_TowerEnemySpawner>();
         _normalUnit = GetOrAddChildComponent<Multi_NormalUnitSpawner>();
         _weapon = GetOrAddChildComponent<Multi_WeaponSpawner>();
-        _effect = GetOrAddChildComponent<EffectSpawner>();
     }
 
     T GetOrAddChildComponent<T>() where T : Component

0fe5f9f30627c10f1c2617e322863934921bc03f
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2023-01-27 13:50:08 +0900

    UI class, color 순으로 정렬 및 개수 제한 구현

--- Assets/0_Multi/1_Script/3_UI/Contents/Paint/UnitTrakerSortByCombineable.cs
+++ Assets/0_Multi/1_Script/3_UI/Contents/Paint/UnitTrakerSortByCombineable.cs
@@ -5,13 +5,19 @@ using System.Linq;
 
 public class UnitTrakerSortByCombineable : UI_UnitTrackerParent
 {
+    readonly int MAX_UI_COUNT = 4;
     protected override void SortTrackers(UnitFlags flag)
     {
         var combineableUnitFalgs = new UnitCombineSystem().GetCombinableUnitFalgs(Multi_UnitManager.Instance.Master.GetUnits(Multi_Data.instance.Id).Select(x => x.UnitFlags));
         if (combineableUnitFalgs.Count() == 0) return;
-        foreach (var unitFlag in combineableUnitFalgs)
+        foreach (var unitFlag in SortUnitFlags(combineableUnitFalgs).Take(MAX_UI_COUNT))
             Managers.UI.MakeSubItem<UI_UnitTracker>(transform).SetInfo(unitFlag);
     }
+
+    IEnumerable<UnitFlags> SortUnitFlags(IEnumerable<UnitFlags> flags)
+        => flags
+            .OrderBy(x => x.ClassNumber)
+            .ThenBy(x => x.ColorNumber);
 
     void OnDisable()
     {

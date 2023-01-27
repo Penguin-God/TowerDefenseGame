2c8b479a2f3d8547bb6b8ccfbfc1e796e2ae7fa1
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2023-01-27 17:55:47 +0900

    적군의 성 튜토리얼 중 콜라이더로 열리는 상점 상호작용 막음

--- Assets/5_Tutorial/Controllers/Tutorial_Tower.cs
+++ Assets/5_Tutorial/Controllers/Tutorial_Tower.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class Tutorial_Tower : TutorialController
 {
@@ -8,6 +9,7 @@ public class Tutorial_Tower : TutorialController
     readonly string ORANGE_SOWRDMAN_UI_NAME = "TutorialOrange";
     protected override void AddTutorials()
     {
+        AddActionCommend(() => SetShopEnable(false));
         AddReadCommend("여기는 적군의 성이 있는 진영입니다.");
         AddUI_HighLightCommend("가운데에 있는 성을 부셔서 보상을 얻기도 하고", "TowerFocus");
         AddUI_HighLightCommend("상점에서 여러가지 상품을 사기도 하는 곳입니다.", "ShopFocus");
@@ -24,7 +26,15 @@ public class Tutorial_Tower : TutorialController
         AddClickCommend("적군의 성으로 버튼을 클릭하면\n해당 유닛이 적군의 성으로 이동해 성을 공격합니다.", "Unit World Changed Button");
         AddClickCommend("적군의 성으로 가서 보낸 유닛을 확인하세요.", "StoryWolrd_EnterButton");
         AddReadCommend("이제 성이 부서지면 보상을 획득할 수 있습니다.\n하지만 적군의 성을 부수지 못하면 유닛만 낭비할 뿐이니\n잘 생각해서 유닛을 보내시기 바랍니다.");
+        AddActionCommend(() => SetShopEnable(true));
     }
 
     protected override bool TutorialStartCondition() => Managers.Camera.IsLookEnemyTower && Managers.Camera.IsLookOtherWolrd == false;
+    void SetShopEnable(bool isEnabled)
+    {
+        FindObjectsOfType<ShopObject>()
+            .Select(x => x.GetComponent<BoxCollider>())
+            .ToList()
+            .ForEach(x => x.enabled = isEnabled);
+    }
 }

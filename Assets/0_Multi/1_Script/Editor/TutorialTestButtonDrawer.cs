7b1d1090c219b7768f6c76987ca02254145c7dfe
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2023-01-28 12:17:52 +0900

    코드 정리, 튜토리얼 테스트 환경 구축

---
+++ Assets/0_Multi/1_Script/Editor/TutorialTestButtonDrawer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(TutorialTester))]
+public class TutorialTestButtonDrawer : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        TutorialTester tester = (TutorialTester)target;
+        if (GUILayout.Button("기본")) tester.AddTutorial<Tutorial_Basic>();
+        if (GUILayout.Button("상대방 진영")) tester.AddTutorial<Tutorial_OtherPlayer>();
+        if (GUILayout.Button("적군의 성")) tester.AddTutorial<Tutorial_Tower>();
+        if (GUILayout.Button("보스")) tester.AddTutorial<Tutorial_Boss>();
+        if (GUILayout.Button("조합")) tester.AddTutorial<Tutorial_Combine>();
+    }
+}
--- Assets/5_Tutorial/TutorialFuntions.cs
+++ Assets/5_Tutorial/TutorialFuntions.cs
@@ -8,8 +8,6 @@ public class TutorialFuntions : MonoBehaviour
     [SerializeField] Light subLight = null;
     [SerializeField] float mainLigth_OffIntensity = 0f;
 
-    [SerializeField] Light spotLight = null;
-
     public void OffLigth()
     {
         Time.timeScale = 0;
@@ -20,44 +18,13 @@ public class TutorialFuntions : MonoBehaviour
     public void OnLigth()
     {
         Time.timeScale = 1;
-        spotLight.gameObject.SetActive(false);
         mainLight.intensity = 1f;
         subLight.intensity = 0.3f;
-    }
-
-    public void Set_SpotLight(Vector3 spot_position)
-    {
-        spotLight.gameObject.SetActive(true);
-        spotLight.transform.position = spot_position + Vector3.up * 5;
-    }
-
-
-    [SerializeField] GameObject lightFocus_UI = null;
-    public void SetBlindUI(RectTransform rect_tf)
-    {
-        lightFocus_UI.SetActive(true);
-        RectTransform rect = lightFocus_UI.GetComponent<RectTransform>();
-        rect.pivot = rect_tf.pivot;
-        rect.anchorMin = rect_tf.anchorMin;
-        rect.anchorMax = rect_tf.anchorMax;
-        rect.position = rect_tf.position;
-
-        rect.sizeDelta = rect_tf.sizeDelta;
-        Time.timeScale = 0;
-    }
-
-    public void Reset_FocusUI()
-    {
-        if (lightFocus_UI == null) return;
-        lightFocus_UI.SetActive(false);
-        lightFocus_UI.GetComponent<RectTransform>().sizeDelta = Vector2.zero;
     }
 
     public void GameProgress()
     {
         Time.timeScale = 1;
-        spotLight.gameObject.SetActive(false);
-        Reset_FocusUI();
         SetAllButton(true);
         OnLigth();
     }

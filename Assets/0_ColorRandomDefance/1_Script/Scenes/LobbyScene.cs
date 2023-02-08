f7be14646a260b640220a5a96bb565d9b84c2c6b
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2023-02-08 18:52:54 +0900

    빛 굽기, 전체 화면 지원

--- Assets/0_ColorRandomDefance/1_Script/Scenes/LobbyScene.cs
+++ Assets/0_ColorRandomDefance/1_Script/Scenes/LobbyScene.cs
@@ -4,5 +4,27 @@ using UnityEngine;
 
 public class LobbyScene : BaseScene
 {
+    bool _isFullScreen;
+    protected override void Init()
+    {
+        Screen.SetResolution(1920, 1080, true);
+        _isFullScreen = true;
+    }
 
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (_isFullScreen)
+            {
+                Screen.SetResolution(800, 480, false);
+                _isFullScreen = false;
+            }
+            else
+            {
+                Screen.SetResolution(1920, 1080, true);
+                _isFullScreen = true;
+            }
+        }
+    }
 }

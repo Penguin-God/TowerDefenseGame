32476f705684d3a645877cd0333b1c2168d6c070
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2023-02-14 19:57:22 +0900

    유닛 스탯 변경 테스트 컴파일도 안되는 단계

--- Assets/1_Test/Testes/DataChangeTester.cs
+++ Assets/1_Test/Testes/DataChangeTester.cs
@@ -8,7 +8,7 @@ public class DataChangeTester
 {
     public void TestChangeUnitAllData()
     {
-        Log("유닛 전체 변경 테스트!!");
+        Log("유닛 스탯 전체 변경 테스트!!");
         var dataManager = new DataManager();
         dataManager.Init();
         var _unitData = dataManager.Unit;
@@ -20,5 +20,22 @@ public class DataChangeTester
 
         for (int i = 0; i < result.Count; i++)
             Assert(result[i] == _unitData.UnitStatByFlag.Values.ToList()[i].BossDamage);
+    }
+
+    public void TestChangeUnitData()
+    {
+        Log("유닛 스탯 변경 테스트!!");
+        int testData = 300;
+        var dataManager = new DataManager();
+        dataManager.Init();
+        var _unitData = dataManager.Unit;
+
+        var result = _unitData.UnitStatByFlag.Values.Where(x => x.Flag.UnitColor == UnitColor.Red).Select(x => x.BossDamage).ToList();
+        for (int i = 0; i < result.Count; i++)
+            result[i] = testData;
+        _unitData.ChangeUnitStat(x => x.UnitColor == UnitColor.Red, stat => stat.SetDamage(testData));
+
+        for (int i = 0; i < result.Count; i++)
+            Assert(result[i] == _unitData.UnitStatByFlag.Values.ToList()[i].Damage);
     }
 }

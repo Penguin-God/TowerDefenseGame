9813011f51193ed3957d557a8867bb9087911204
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2023-02-21 19:11:38 +0900

    웨폰 패스 빌더로 전부 대체

--- Assets/0_ColorRandomDefance/1_Script/4_Managers/Spawners/Multi_WeaponSpawner.cs
+++ Assets/0_ColorRandomDefance/1_Script/4_Managers/Spawners/Multi_WeaponSpawner.cs
@@ -3,50 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
-[System.Serializable]
-public struct FolderPoolingData
-{
-    public string folderName;
-    public GameObject[] gos;
-    public int poolingCount;
-}
-
 public class Multi_WeaponSpawner : Multi_SpawnerBase
 {
-    [SerializeField] FolderPoolingData mageballPoolData;
-    [SerializeField] FolderPoolingData mageSkillPoolData;
+    protected override void MasterInit() => PoolWeapons();
 
-    protected override void MasterInit()
+    void PoolWeapons()
     {
-        PoolWeapon();
-
-        InitWeapons(mageballPoolData.gos, mageballPoolData.folderName, mageballPoolData.poolingCount);
-        InitWeapons(mageSkillPoolData.gos, mageSkillPoolData.folderName, mageSkillPoolData.poolingCount);
-    }
-
-    void PoolWeapon()
-    {
-        var poolWeaponUnitClassArr = new UnitClass[] { UnitClass.Archer, UnitClass.Spearman };
         var unitClassByWeaponPoolingCount = new Dictionary<UnitClass, int>()
         {
             { UnitClass.Archer, 20 },
             { UnitClass.Spearman, 2 },
+            { UnitClass.Mage, 0 },
         };
 
         foreach (UnitColor color in Enum.GetValues(typeof(UnitColor)))
         {
-            foreach (UnitClass unitClass in poolWeaponUnitClassArr)
-                CreatePoolGroup(PathBuilder.BuildUnitWeaponPath(new UnitFlags(color, unitClass)), unitClassByWeaponPoolingCount[unitClass]);
+            foreach (var classCountPair in unitClassByWeaponPoolingCount)
+                CreatePoolGroup(PathBuilder.BuildUnitWeaponPath(new UnitFlags(color, classCountPair.Key)), classCountPair.Value);
+        }
+
+        foreach (UnitColor color in Enum.GetValues(typeof(UnitColor)))
+        {
+            if (color == UnitColor.White) continue;
+            CreatePoolGroup(PathBuilder.BuildMageSkillEffectPath(color), 0);
         }
     }
-
-    [SerializeField] string _rootPath;
-    void InitWeapons(GameObject[] gos, string folderName, int count)
-    {
-        for (int i = 0; i < gos.Length; i++)
-            CreatePoolGroup(BuildPath(_rootPath, folderName, gos[i]), count);
-    }
-    string BuildPath(string rooPath, string folderName, GameObject go) => $"{rooPath}/{folderName}/{go.name}";
 
     public GameObject Spawn(string path, Vector3 spawnPos) => Managers.Multi.Instantiater.PhotonInstantiate($"Weapon/{path}", spawnPos);
 }

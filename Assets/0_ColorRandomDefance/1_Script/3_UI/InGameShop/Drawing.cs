2c9d097ae7b7eeb686248feed72884390517c6c4
jangjo123 <cjkh1324@naver.com>
2023-03-05 23:42:13 +0900

    뽑기 시스템 1 (랜덤한 스킬의 exp추가)

---
+++ Assets/0_ColorRandomDefance/1_Script/3_UI/InGameShop/Drawing.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Linq;
+
+public class Drawing : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+    
+    public void DrawingSkills()
+    {
+        List<int> numbers = new List<int>();
+
+        for (int i = 1; i <= 10; i++)
+        {
+            numbers.Add(i);
+        }
+
+        List<int> selectedNumbers = new List<int>();
+        int count = 0;
+
+        while (count < 4)
+        {
+            int index = UnityEngine.Random.Range(0, numbers.Count);
+
+            int number = numbers[index];
+
+            if (!selectedNumbers.Contains(number))
+            {
+                selectedNumbers.Add(number);
+                count++;
+            }
+
+            numbers.RemoveAt(index);
+
+            Managers.ClientData.GetExp((SkillType)number, 10);
+        }
+
+        Debug.Log(string.Join(", ", selectedNumbers));
+
+
+    }
+
+}

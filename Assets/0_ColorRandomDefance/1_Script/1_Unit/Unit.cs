6df9687ec371213886ec494b2aff1899d029c877
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2023-03-10 18:31:14 +0900

    유닛 도메인 클래스 생성

---
+++ Assets/0_ColorRandomDefance/1_Script/1_Unit/Unit.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Unit
+{
+    readonly UnitFlags _flag;
+    public Unit(UnitFlags flag) => _flag = flag;
+
+    public UnitStat Stat;
+
+    public event Action<Unit> OnDead;
+    public void Dead()
+    {
+        OnDead?.Invoke(this);
+        OnDead = null;
+    }
+}

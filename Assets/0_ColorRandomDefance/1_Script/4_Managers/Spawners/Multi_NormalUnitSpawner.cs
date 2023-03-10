41e30db1eb3eaccb93f17f53a36be3df552f0939
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2023-03-10 14:25:52 +0900

    새로운 날개짓을 위한 뻘짓

--- Assets/0_ColorRandomDefance/1_Script/4_Managers/Spawners/Multi_NormalUnitSpawner.cs
+++ Assets/0_ColorRandomDefance/1_Script/4_Managers/Spawners/Multi_NormalUnitSpawner.cs
@@ -7,28 +7,33 @@ public class Multi_NormalUnitSpawner : MonoBehaviourPun
 {
     protected readonly ResourcesPathBuilder PathBuilder = new ResourcesPathBuilder();
 
-    public void Spawn(UnitFlags flag) => Spawn(flag.ColorNumber, flag.ClassNumber);
-    public void Spawn(int colorNum, int classNum) => Spawn(new UnitFlags(colorNum, classNum), PlayerIdManager.Id);
-    public void Spawn(UnitFlags flag, byte id) => Spawn(flag, Vector3.zero, Quaternion.identity, id);
-    public void Spawn(UnitFlags flag, Vector3 spawnPos, Quaternion rotation, byte id)
+    public Multi_TeamSoldier Spawn(UnitFlags flag) => Spawn(flag.ColorNumber, flag.ClassNumber);
+    public Multi_TeamSoldier Spawn(int colorNum, int classNum) => Spawn(new UnitFlags(colorNum, classNum), PlayerIdManager.Id);
+    public Multi_TeamSoldier Spawn(UnitFlags flag, byte id) => Spawn(flag, Vector3.zero, Quaternion.identity, id);
+    public Multi_TeamSoldier Spawn(UnitFlags flag, Vector3 spawnPos, Quaternion rotation, byte id)
     {
+        if (PhotonNetwork.IsMasterClient)
+            return RPCSpawn(flag, spawnPos, rotation, id);
+
         if (rotation == Quaternion.identity || spawnPos == Vector3.zero)
             photonView.RPC(nameof(RPCSpawn), RpcTarget.MasterClient, flag, id);
         else
             photonView.RPC(nameof(RPCSpawn), RpcTarget.MasterClient, flag, spawnPos, rotation, id);
+        return null;
     }
 
     Vector3 GetUnitSpawnPos(int id) => Multi_WorldPosUtility.Instance.GetUnitSpawnPositon(id);
 
     // MasterOnly
     [PunRPC]
-    void RPCSpawn(UnitFlags flag, byte id) => RPCSpawn(flag, GetUnitSpawnPos(id), Quaternion.identity, id);
+    Multi_TeamSoldier RPCSpawn(UnitFlags flag, byte id) => RPCSpawn(flag, GetUnitSpawnPos(id), Quaternion.identity, id);
 
     [PunRPC]
-    void RPCSpawn(UnitFlags flag, Vector3 spawnPos, Quaternion rotation, byte id)
+    Multi_TeamSoldier RPCSpawn(UnitFlags flag, Vector3 spawnPos, Quaternion rotation, byte id)
     {
         var unit = Managers.Multi.Instantiater.PhotonInstantiate(PathBuilder.BuildUnitPath(flag), spawnPos, rotation, id).GetComponent<Multi_TeamSoldier>();
         unit.Spawn();
         Multi_UnitManager.Instance.Master.AddUnit(unit);
+        return unit;
     }
 }
--- Assets/1_Test/PlayModeTests/UnitStatChangeTests.cs
+++ Assets/1_Test/PlayModeTests/UnitStatChangeTests.cs
@@ -9,26 +9,17 @@ namespace Tests
 {
     public class UnitStatChangeTests
     {
-        [OneTimeSetUp]
-        public void OneTimeSetup()
-        {
-            new GameObject("MultiHelper").AddComponent<MultiDevelopHelper>().EditorConnect();
-        }
-
         [UnityTest]
         public IEnumerator AddUnitDamage()
         {
-            Multi_SpawnManagers.NormalUnit.gameObject.AddComponent<PhotonView>();
-            Multi_UnitManager.Instance.gameObject.AddComponent<PhotonView>();
-            Multi_SpawnManagers.NormalUnit.Spawn(UnitFlags.RedSowrdman);
-
             const int ADD_AMOUNT = 100;
-            int result = Multi_GameManager.Instance.UnitDamageManager.GetUnitDamage(UnitFlags.RedSowrdman) + ADD_AMOUNT;
-            Multi_GameManager.Instance.UnitStatChanger.AddUnitDamage(ADD_AMOUNT);
-
-
+            var unit = Multi_SpawnManagers.NormalUnit.Spawn(UnitFlags.RedSowrdman);
             yield return null;
-            Assert.AreEqual(result, Multi_UnitManager.Instance.FindUnit(0, UnitFlags.RedSowrdman).Stat.Damage);
+
+            Multi_UnitManager.Instance.Stat.AddUnitDamage(ADD_AMOUNT);
+
+            int result = Multi_GameManager.Instance.UnitDamageManager.GetUnitDamage(UnitFlags.RedSowrdman) + ADD_AMOUNT;
+            Assert.AreEqual(result, unit.Stat.Damage);
             Assert.AreEqual(result, Multi_GameManager.Instance.UnitDamageManager.GetUnitDamage(UnitFlags.RedSowrdman));
         }
     }

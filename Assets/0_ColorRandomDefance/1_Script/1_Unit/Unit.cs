330abe38362d6cd7d4d1e3301cb371b50cd7dadf
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2023-03-11 14:02:12 +0900

    유닛 테스트 관련 스켈레톤 코드 작성

--- Assets/0_ColorRandomDefance/1_Script/1_Unit/Unit.cs
+++ Assets/0_ColorRandomDefance/1_Script/1_Unit/Unit.cs
@@ -8,50 +8,20 @@ public class Unit
     private readonly UnitFlags _unitFlags;
     public UnitFlags UnitFlags => _unitFlags;
 
-    public int Damage { get; private set; }
-    public int BossDamage { get; private set; }
-    public float Speed { get; private set; }
-    public float AttackDelayTime { get; private set; }
-    public float AttackRange { get; private set; }
+    readonly UnitDamageInfo _damageInfo;
+    public UnitDamageInfo DamageIndo => _damageInfo;
 
     public Action<Multi_Enemy> OnHit;
     public Action<Multi_Enemy> OnPassiveHit;
     public event Action<Unit> OnDead;
 
-    public Unit(UnitStat stat)
-    {
-        _unitFlags = stat.Flag;
-        LoadStat(stat);
-        OnHit += AttackEnemy;
-    }
-
-    public Unit(UnitFlags flag)
+    public Unit(UnitFlags flag, int damage, int bossDamage)
     {
         _unitFlags = flag;
+        _damageInfo = new UnitDamageInfo(damage, bossDamage);
     }
 
-    void LoadStat(UnitStat stat)
-    {
-        Damage = stat.Damage;
-        BossDamage = stat.BossDamage;
-        Speed = stat.Speed;
-        AttackDelayTime = stat.AttackDelayTime;
-        AttackRange = stat.AttackRange;
-    }
+    public Unit(UnitFlags flag) => new Unit(flag, 0, 0);
 
     public void Dead() => OnDead?.Invoke(this);
-
-    void AttackEnemy(Multi_Enemy enemy) // Boss랑 쫄병 구분해서 대미지 적용
-    {
-        if (enemy.enemyType == EnemyType.Normal) AttackEnemy(enemy, Damage);
-        else AttackEnemy(enemy, BossDamage);
-    }
-
-    void AttackEnemy(Multi_Enemy enemy, int damage) => enemy.OnDamage(damage);
-
-    protected void SkillAttackToEnemy(Multi_Enemy enemy, int damage)
-    {
-        enemy.OnDamage(damage, isSkill: true);
-        OnPassiveHit?.Invoke(enemy);
-    }
 }

2979fdeb4a8a96ff5322bb787c876203ddd35e2f
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2023-03-11 14:29:37 +0900

    멀티 서비스 중개자 생성

---
+++ Assets/0_ColorRandomDefance/1_Script/4_Managers/MultiServiceMidiator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiServiceMidiator
+{
+       
+}
+
+
+public class MultiService<T>
+{
+    T[] ts = new T[2];
+    T GetServiece() => ts[PlayerIdManager.Id];
+    T GetServiece(byte id) => ts[id];
+}

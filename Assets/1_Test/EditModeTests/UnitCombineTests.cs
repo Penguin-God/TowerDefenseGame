7ffa5f68fd3075efdbd36d0446e92a95f9e75223
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2023-03-16 12:40:04 +0900

    테스트 스크립트 삭제 및 생성

--- Assets/1_Test/EditModeTests/UnitCombineTests.cs
+++ Assets/1_Test/EditModeTests/UnitCombineTests.cs
@@ -7,33 +7,8 @@ using UnityEngine.TestTools;
 
 namespace Tests
 {
-    public class UnitManagerTests
+    public class UnitCombineTests
     {
-        //[Test]
-        //public void 유닛이_매니저에_추가되면_찾을_수_있어야함()
-        //{
-        //    var sut = new UnitManager();
-
-        //    sut.RegisterUnit(new Unit(UnitFlags.RedSowrdman));
-
-        //    Assert.AreEqual(1, sut.UnitCount);
-        //    Assert.NotNull(sut.GetUnit(UnitFlags.RedSowrdman));
-        //    Assert.IsNull(sut.GetUnit(UnitFlags.BlueSowrdman));
-        //}
-
-        //[Test]
-        //public void 유닛이_죽으면_매니저에서_삭제되어야함()
-        //{
-        //    var sut = new UnitManager();
-        //    var unit = new Unit(UnitFlags.RedSowrdman);
-        //    sut.RegisterUnit(unit);
-
-        //    unit.Dead();
-
-        //    Assert.AreEqual(0, sut.UnitCount);
-        //    Assert.IsNull(sut.GetUnit(UnitFlags.RedSowrdman));
-        //}
-
         [Test]
         public void CombineResultTrue()
         {

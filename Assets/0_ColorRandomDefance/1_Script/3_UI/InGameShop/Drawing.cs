8431419f34b4356fd992244911edf519c1014333
jangjo123 <cjkh1324@naver.com>
2023-03-19 23:36:52 +0900

    상자 종류

--- Assets/0_ColorRandomDefance/1_Script/3_UI/InGameShop/Drawing.cs
+++ Assets/0_ColorRandomDefance/1_Script/3_UI/InGameShop/Drawing.cs
@@ -6,14 +6,14 @@ using System.Linq;
 
 public class Drawing : MonoBehaviour
 {
-    public static List<int> NumberChoice()
+    public List<int> NumberChoice()
     {
         List<int> nums = new List<int>();
         nums.Add(UnityEngine.Random.Range(50, 81)); 
         nums.Add(UnityEngine.Random.Range(15, 41));
         nums.Add(UnityEngine.Random.Range(10, 16)); 
         nums.Add(100 - nums[0] - nums[1] - nums[2]); 
-
+        
         nums.Sort();
         nums.Reverse();
 
@@ -22,6 +22,7 @@ public class Drawing : MonoBehaviour
 
     public List<int> DrawingSkills()
     {
+        //List<int> randomInt = NumberChoice();
         List<int> numbers = new List<int>();
         
         for (int i = 1; i <= Enum.GetValues(typeof(SkillType)).Length; i++)
@@ -41,13 +42,11 @@ public class Drawing : MonoBehaviour
             if (!selectedNumbers.Contains(number))
             {
                 selectedNumbers.Add(number);
-                Managers.ClientData.GetExp((SkillType)number, NumberChoice()[count]);
+                //Managers.ClientData.GetExp((SkillType)number, randomInt[count]);
                 count++;
             }
 
             numbers.RemoveAt(index);
-
-            
         }
 
         Debug.Log(string.Join(", ", selectedNumbers));
@@ -58,6 +57,18 @@ public class Drawing : MonoBehaviour
     public void TestButton()
     {
         print(DrawingSkills()[0]);
+    }
+
+    public void Test()
+    {
+        List<int> itemCountList = NumberChoice();
+        List<int> selectedNumbers = DrawingSkills();
+
+        for (int i = 0; i <= selectedNumbers.Count; i++)
+        {
+            Managers.ClientData.GetExp((SkillType)selectedNumbers[i], itemCountList[i]);
+            print(selectedNumbers[i] + itemCountList[i]); // 임시
+        }
     }
 
 }

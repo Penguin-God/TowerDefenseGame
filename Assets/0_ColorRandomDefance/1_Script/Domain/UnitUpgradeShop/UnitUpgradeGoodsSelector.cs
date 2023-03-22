09acbef02c533c98acbf1b6815c4b66e88c0edd7
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2023-03-22 18:10:15 +0900

    유닛 업그레이드 상점 상품 선택 테스트 통과

---
+++ Assets/0_ColorRandomDefance/1_Script/Domain/UnitUpgradeShop/UnitUpgradeGoodsSelector.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public enum UnitUpgradeType
+{
+    Value,
+    Scale,
+}
+
+public struct UnitUpgradeGoods
+{
+    public UnitUpgradeType UpgradeType { get; private set; }
+    public UnitColor TargetColor { get; private set; }
+
+    public UnitUpgradeGoods(UnitUpgradeType upgradeType, UnitColor color)
+    {
+        UpgradeType = upgradeType;
+        TargetColor = color;
+    }
+}
+
+public class UnitUpgradeGoodsSelector
+{
+    readonly int GOODS_COUNT = 3;
+    public IEnumerable<UnitUpgradeGoods> SelectGoods()
+    {
+        var allGoods = GetAllGoods().ToList();
+        var result = new List<UnitUpgradeGoods>();
+        for (int i = 0; i < GOODS_COUNT; i++)
+        {
+            int randNum = UnityEngine.Random.Range(0, allGoods.Count);
+            result.Add(allGoods[randNum]);
+            allGoods.RemoveAt(randNum);
+        }
+        return result;
+    }
+
+    IEnumerable<UnitUpgradeGoods> GetAllGoods()
+        => Enum.GetValues(typeof(UnitUpgradeType))
+        .Cast<UnitUpgradeType>()
+        .SelectMany(upgradeType => Enum.GetValues(typeof(UnitColor))
+        .Cast<UnitColor>()
+        .Where(x => x != UnitColor.White && x != UnitColor.Black)
+        .Select(color => new UnitUpgradeGoods(upgradeType, color)));
+}
---
+++ Assets/1_Test/EditModeTests/UnitUpgradeShopTests.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using System.Linq;
+using System;
+using System.Runtime.InteropServices;
+using System.Data.Common;
+
+namespace Tests
+{
+    public class UnitUpgradeShopTests
+    {
+        [Test]
+        public void 유닛_업그레이드_상점은_서로_다른_상품_3개를_제공함()
+        {
+            var selector = new UnitUpgradeGoodsSelector();
+
+            var result = selector.SelectGoods();
+
+            Assert.AreEqual(3, result.Count());
+            Assert.AreEqual(3, result.Distinct().Count());
+        }
+
+        [Test]
+        public void 상품이_랜덤하게_고루_뽑히는지_확인()
+        {
+            var selector = new UnitUpgradeGoodsSelector();
+            var counter = new Dictionary<UnitUpgradeGoods, int>();
+            int selectCount = 10000;
+
+            for (int i = 0; i < selectCount; i++)
+            {
+                foreach (var goods in selector.SelectGoods())
+                {
+                    if (counter.ContainsKey(goods))
+                        counter[goods]++;
+                    else
+                        counter.Add(goods, 1);
+                }
+            }
+
+            int allGoodsCount = Enum.GetValues(typeof(UnitUpgradeType)).Length *
+                Enum.GetValues(typeof(UnitColor)).Cast<UnitColor>().Where(x => x != UnitColor.White && x != UnitColor.Black).Count();
+            Assert.AreEqual(allGoodsCount, counter.Count);
+
+            // 확률 분포 확인
+            float expectRate = 1f / allGoodsCount;
+            float delta = 0.01f;
+            foreach (var item in counter)
+            {
+                float selectRate = item.Value / 3f / (float)selectCount;
+                Assert.IsTrue(Mathf.Abs(selectRate - expectRate) < delta);
+            }
+        }
+    }
+}

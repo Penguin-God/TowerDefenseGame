a8d50e1d02a24e40835f6d6922e69b72ca755574
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2023-04-26 19:07:17 +0900

    타워에 레벨 나타내는 텍스트 추가

--- Assets/0_ColorRandomDefance/1_Script/2_Enemy/Multi_EnemyTower.cs
+++ Assets/0_ColorRandomDefance/1_Script/2_Enemy/Multi_EnemyTower.cs
@@ -2,25 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using TMPro;
 
 public class Multi_EnemyTower : Multi_Enemy
 {
     [SerializeField] int _level;
     public int Level => _level;
     public BossData TowerData { get; private set; }
-
+    
     [PunRPC]
     protected override void OnPoison(int poisonPercent, int poisonCount, float poisonDelay, int maxDamage, bool isSkill) { }
 
-    protected override void Init()
-    {
-        base.Init();
-        enemyType = EnemyType.Tower;
-    }
-
     public void Spawn(int level)
     {
+        enemyType = EnemyType.Tower;
         _level = level;
+        gameObject.GetComponentInChildren<TextMeshProUGUI>().text = level.ToString();
         TowerData = Managers.Data.TowerDataByLevel[_level];
         SetStatus_RPC(TowerData.Hp, TowerData.Speed, false);
     }

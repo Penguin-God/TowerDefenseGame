1bd86c67316e47799f46405b006b7b664b0447bd
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2023-05-18 18:31:46 +0900

    feat: 멀티 전용 유닛 컨트롤러 제작. 실사용 필요

---
+++ Assets/0_ColorRandomDefance/1_Script/Multi/UnitController.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using System.Linq;
+
+public abstract class UnitController : MonoBehaviourPun, IUnitContorller
+{
+    protected UnitCombineSystem _combineSystem;
+    protected void Init(DataManager data)
+    {
+        _combineSystem = new UnitCombineSystem(data.CombineConditionByUnitFalg);
+    }
+
+    public bool TryCombine(UnitFlags targetFlag)
+    {
+        if (CanCombine(targetFlag))
+        {
+            Combine(targetFlag, PlayerIdManager.Id);
+            return true;
+        }
+        return false;
+    }
+
+    public virtual bool CanCombine(UnitFlags targetFlag) => true;
+
+    [PunRPC]
+    protected abstract void Combine(UnitFlags targetFlag, byte id);
+}
+
+
+public class ServerUnitController : UnitController
+{
+    UnitManager _unit;
+    public void Init(DataManager data, UnitManager unit)
+    {
+        base.Init(data);
+        _unit = unit;
+    }
+
+    public override bool CanCombine(UnitFlags targetFlag) => _combineSystem.CheckCombineable(targetFlag, _unit.GetUnitCount);
+
+    [PunRPC]
+    protected override void Combine(UnitFlags targetFlag, byte id) => photonView.RPC(nameof(Combine), RpcTarget.MasterClient, targetFlag, id);
+}
+
+public class ClientUnitController : UnitController
+{
+    ServerManager _server;
+
+    public void Init(DataManager data, ServerManager server)
+    {
+        base.Init(data);
+        _server = server;
+    }
+
+    [PunRPC]
+    protected override void Combine(UnitFlags targetFlag, byte id)
+    {
+        if(_combineSystem.CheckCombineable(targetFlag, _server.GetUnits(id).Select(x => x.UnitFlags)) == false)
+            return;
+
+        foreach (var needFlag in _combineSystem.GetNeedFlags(targetFlag))
+            _server.GetUnits(id).Where(x => x.UnitFlags == needFlag).First().Dead();
+
+        Multi_SpawnManagers.NormalUnit.Spawn(targetFlag, id);
+    }
+}

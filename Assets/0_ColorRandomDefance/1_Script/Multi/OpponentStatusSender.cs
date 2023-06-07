4bf7f65be4963313b32a8ae1a9695041a9355229
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2023-06-07 17:52:36 +0900

    refactor: 죽은 코드 제거

--- Assets/0_ColorRandomDefance/1_Script/Multi/OpponentStatusSender.cs
+++ Assets/0_ColorRandomDefance/1_Script/Multi/OpponentStatusSender.cs
@@ -3,22 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 
-public class OpponentStatusSynchronizer
-{
-    public RPCAction<byte, UnitClass, byte> OnOtherUnitCountChanged = new RPCAction<byte, UnitClass, byte>();
-    public RPCAction<byte> OnOtherUnitMaxCountChanged = new RPCAction<byte>();
-
-    public OpponentStatusSynchronizer()
-    {
-        Managers.Unit.OnUnitCountChangeByClass += SendUnitCountDataToOpponent;
-        Multi_GameManager.Instance.BattleData.OnMaxUnitChanged += SendUnitMaxCountDataToOpponent;
-    }
-
-    void SendUnitCountDataToOpponent(UnitClass unitClass, int count)
-        => OnOtherUnitCountChanged?.RaiseToOther((byte)Managers.Unit.CurrentUnitCount, unitClass, (byte)count);
-    void SendUnitMaxCountDataToOpponent(int count) => OnOtherUnitMaxCountChanged?.RaiseToOther((byte)count);
-}
-
 public class OpponentStatusSender : MonoBehaviourPun
 {
     BattleEventDispatcher _dispatcher;

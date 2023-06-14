e63ac703e5e24574f8c1dcbb7332ace40828e763
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2023-06-14 16:53:00 +0900

    test: namespace 통일

--- Assets/1_Test/EditModeTests/TextValueSystemTests.cs
+++ Assets/1_Test/EditModeTests/TextValueSystemTests.cs
@@ -5,50 +5,53 @@ using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
 
-public class TextValueSystemTests
+namespace Tests
 {
-    UnitFlags RedArcher = new UnitFlags(0, 1);
-    UnitFlags VioaltSpearman = new UnitFlags(5, 2);
-
-    [Test]
-    public void 유닛_플래그에_따른_데미지_키를_생성해야_함()
+    public class TextValueSystemTests
     {
-        var sut = new UnitKeyBuilder();
+        UnitFlags RedArcher = new UnitFlags(0, 1);
+        UnitFlags VioaltSpearman = new UnitFlags(5, 2);
 
-        Assert.AreEqual("{%At01}", sut.BuildAttackKey(RedArcher));
-        Assert.AreEqual("{%BAt01}", sut.BuildBossAttackKey(RedArcher));
-    }
+        [Test]
+        public void 유닛_플래그에_따른_데미지_키를_생성해야_함()
+        {
+            var sut = new UnitKeyBuilder();
 
-    [Test]
-    public void 패시브는_개수만큼의_키를_생성해야_함()
-    {
-        var sut = new UnitKeyBuilder();
-        const int PassiveCount = 4;
+            Assert.AreEqual("{%At01}", sut.BuildAttackKey(RedArcher));
+            Assert.AreEqual("{%BAt01}", sut.BuildBossAttackKey(RedArcher));
+        }
 
-        var result = sut.BuildPassiveKeys(VioaltSpearman, PassiveCount).ToList();
+        [Test]
+        public void 패시브는_개수만큼의_키를_생성해야_함()
+        {
+            var sut = new UnitKeyBuilder();
+            const int PassiveCount = 4;
 
-        Assert.AreEqual(result.Count, PassiveCount);
-        Assert.AreEqual("{%Pa520}", result[0]);
-        Assert.AreEqual("{%Pa521}", result[1]);
-        Assert.AreEqual("{%Pa522}", result[2]);
-        Assert.AreEqual("{%Pa523}", result[3]);
-    }
+            var result = sut.BuildPassiveKeys(VioaltSpearman, PassiveCount).ToList();
 
-    [Test]
-    public void 모든_키를_생성해야_함()
-    {
-        var sut = new UnitKeyBuilder();
-        const int PassiveCount = 4;
+            Assert.AreEqual(result.Count, PassiveCount);
+            Assert.AreEqual("{%Pa520}", result[0]);
+            Assert.AreEqual("{%Pa521}", result[1]);
+            Assert.AreEqual("{%Pa522}", result[2]);
+            Assert.AreEqual("{%Pa523}", result[3]);
+        }
 
-        var result = sut.BuildAllKeys(VioaltSpearman, PassiveCount);
+        [Test]
+        public void 모든_키를_생성해야_함()
+        {
+            var sut = new UnitKeyBuilder();
+            const int PassiveCount = 4;
 
-        Assert.AreEqual(result.Count(), 6);
+            var result = sut.BuildAllKeys(VioaltSpearman, PassiveCount);
 
-        CollectionAssert.Contains(result, "{%At52}");
-        CollectionAssert.Contains(result, "{%BAt52}");
-        CollectionAssert.Contains(result, "{%Pa520}");
-        CollectionAssert.Contains(result, "{%Pa521}");
-        CollectionAssert.Contains(result, "{%Pa522}");
-        CollectionAssert.Contains(result, "{%Pa523}");
+            Assert.AreEqual(result.Count(), 6);
+
+            CollectionAssert.Contains(result, "{%At52}");
+            CollectionAssert.Contains(result, "{%BAt52}");
+            CollectionAssert.Contains(result, "{%Pa520}");
+            CollectionAssert.Contains(result, "{%Pa521}");
+            CollectionAssert.Contains(result, "{%Pa522}");
+            CollectionAssert.Contains(result, "{%Pa523}");
+        }
     }
 }

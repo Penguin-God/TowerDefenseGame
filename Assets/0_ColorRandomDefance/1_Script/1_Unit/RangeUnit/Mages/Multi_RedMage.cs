d5196c3f407b3a6181a101881a97b5af7e7a1f15
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2023-06-16 18:46:10 +0900

    refactor: 컨트롤러 MonoBehaviour로 변경

--- Assets/0_ColorRandomDefance/1_Script/1_Unit/RangeUnit/Mages/Multi_RedMage.cs
+++ Assets/0_ColorRandomDefance/1_Script/1_Unit/RangeUnit/Mages/Multi_RedMage.cs
@@ -6,10 +6,11 @@ public class Multi_RedMage : Multi_Unit_Mage
 {
     [SerializeField] float _damRate;
     [SerializeField] float meteorStunTime;
-    MeteorController _meteorController = new MeteorController();
+    MeteorController _meteorController;
 
     public override void SetMageAwake()
     {
+        _meteorController = gameObject.AddComponent<MeteorController>();
         _damRate = skillStats[0];
         meteorStunTime = skillStats[1];
 
@@ -19,7 +20,7 @@ public class Multi_RedMage : Multi_Unit_Mage
 
     [SerializeField] Vector3 meteorPos = (Vector3.up * 30) + (Vector3.forward * 5);
     Vector3 CalculateMeteorSawpnPos() => transform.position + meteorPos;
-    protected override void MageSkile() => StartCoroutine(_meteorController.Co_ShotMeteor(TargetEnemy, CalculateSkillDamage(_damRate), meteorStunTime, CalculateMeteorSawpnPos()));
+    protected override void MageSkile() => _meteorController.ShotMeteor(TargetEnemy, CalculateSkillDamage(_damRate), meteorStunTime, CalculateMeteorSawpnPos());
     protected override void PlaySkillSound() => PlaySound(EffectSoundType.RedMageSkill);
 
     // passive
--- Assets/0_ColorRandomDefance/1_Script/Contorller/MeteorController.cs
+++ Assets/0_ColorRandomDefance/1_Script/Contorller/MeteorController.cs
@@ -3,14 +3,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MeteorController
+public class MeteorController : MonoBehaviour
 {
     readonly string MeteorPath = $"Weapon/MageSkills/Meteor 1";
-    public IEnumerator Co_ShotMeteor(Multi_Enemy target, int hitDamage, float stunTime, Vector3 spawnPos)
+
+    void HitAction(Multi_Enemy target, int hitDamage, float stunTime)
+    {
+        target.OnDamage(hitDamage);
+        target.OnStun_RPC(100, stunTime);
+    }
+
+    public void ShotMeteor(Multi_Enemy target, int hitDamage, float stunTime, Vector3 spawnPos)
+    {
+        Action<Multi_Enemy> hitAction = (_) => HitAction(_, hitDamage, stunTime);
+        StartCoroutine(Co_ShotMeteor(target, hitAction, spawnPos));
+    }
+
+    IEnumerator Co_ShotMeteor(Multi_Enemy target, Action<Multi_Enemy> hitAction, Vector3 spawnPos)
     {
         var meteor = WeaponSpawner.Spawn(MeteorPath, spawnPos).GetComponent<Multi_Meteor>();
-
-        Action<Multi_Enemy> hitAction = (_) => HitAction(_, hitDamage, stunTime);
         Vector3 tempPos = target.transform.position;
         yield return new WaitForSeconds(1f);
 
@@ -18,11 +29,5 @@ public class MeteorController
             meteor.Shot(null, tempPos, hitAction);
         else
             meteor.Shot(target, target.transform.position, hitAction);
-    }
-
-    void HitAction(Multi_Enemy target, int hitDamage, float stunTime)
-    {
-        target.OnDamage(hitDamage);
-        target.OnStun_RPC(100, stunTime);
     }
 }

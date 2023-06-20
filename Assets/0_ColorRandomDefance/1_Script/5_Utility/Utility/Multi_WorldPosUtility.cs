92637104d01d33e18603fa68a38552ecd9e58b12
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2023-06-20 19:07:20 +0900

    test: 유닛 스폰 위치 계산기 테스트 작성 및 통과

--- Assets/0_ColorRandomDefance/1_Script/5_Utility/Utility/Multi_WorldPosUtility.cs
+++ Assets/0_ColorRandomDefance/1_Script/5_Utility/Utility/Multi_WorldPosUtility.cs
@@ -50,3 +50,34 @@ public class Multi_WorldPosUtility : MonoBehaviour
         return _randomPos;
     }
 }
+
+public class WorldSpawnPositionCalculator
+{
+    readonly float WorldSpawnRange; // 정사각형
+
+    readonly float EnemyTowerOffsetZ;
+    readonly float EnemyTowerSpawnRange_X;
+    readonly float EnemyTowerSpawnRange_Z;
+
+    public WorldSpawnPositionCalculator(float worldSpawnRange, float enemyTowerOffsetZ, float enemyTowerSpawnRange_X, float enemyTowerSpawnRange_Z)
+    {
+        WorldSpawnRange = worldSpawnRange;
+        EnemyTowerOffsetZ = enemyTowerOffsetZ;
+        EnemyTowerSpawnRange_X = enemyTowerSpawnRange_X;
+        EnemyTowerSpawnRange_Z = enemyTowerSpawnRange_Z;
+    }
+
+    public Vector3 GetUnitSpawnPositon(Vector3 worldPos) => GetRandomPos_InRange(worldPos, WorldSpawnRange);
+    public Vector3 GetEnemyTower_TP_Position(Vector3 towerPos) 
+        => GetRandomPos_InRange(new Vector3(towerPos.x, towerPos.y, towerPos.z + EnemyTowerOffsetZ), EnemyTowerSpawnRange_X, EnemyTowerSpawnRange_Z);
+
+    Vector3 GetRandomPos_InRange(Vector3 _pivot, float _range) => GetRandomPos_InRange(_pivot, _range, _range);
+    Vector3 GetRandomPos_InRange(Vector3 _pivot, float _xRange, float _zRange)
+    {
+        float _x = Random.Range(_pivot.x - _xRange, _pivot.x + _xRange);
+        float _z = Random.Range(_pivot.z - _zRange, _pivot.z + _zRange);
+        Vector3 _randomPos = new Vector3(_x, _pivot.y, _z);
+        return _randomPos;
+    }
+}
+
---
+++ Assets/1_Test/EditModeTests/SpawnPositionCalculatorTests.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class SpawnPositionCalculatorTests
+{
+    [Test]
+    public void 월드_위치는_지정한_범위_이내에_위치를_반한해야_함()
+    {
+        // Arrange
+        var worldPos = new Vector3(0, 0, 0);
+        var spawnRange = 10f;
+        var calculator = new WorldSpawnPositionCalculator(spawnRange, 0, 0, 0);
+
+        // Act
+        var result = calculator.GetUnitSpawnPositon(worldPos);
+
+        // Assert
+        Assert.That(result.x, Is.InRange(-10f, 10f));
+        Assert.That(result.z, Is.InRange(-10f, 10f));
+    }
+
+    [Test]
+    public void 적군의_성_위치는_OffSet이_적용된_상태에서_지정한_범위_내에_위치를_반환해야_함()
+    {
+        // Arrange
+        var towerPos = new Vector3(0, 0, 0);
+        var offsetZ = -10f;
+        var spawnRangeX = 10f;
+        var spawnRangeZ = 10f;
+        var calculator = new WorldSpawnPositionCalculator(0, offsetZ, spawnRangeX, spawnRangeZ);
+
+        // Act
+        var result = calculator.GetEnemyTower_TP_Position(towerPos);
+
+        // Assert
+        Assert.That(result.x, Is.InRange(-10f, 10f));
+        Assert.That(result.z, Is.InRange(-20f, 0f));
+    }
+}

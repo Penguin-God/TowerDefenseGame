d19f2a39611f8490cc012b13980af91e160749b3
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2023-06-21 19:11:59 +0900

    feat: 몬스터 소환 실시간 반영

--- Assets/0_ColorRandomDefance/1_Script/4_Managers/Spawners/Multi_NormalEnemySpawner.cs
+++ Assets/0_ColorRandomDefance/1_Script/4_Managers/Spawners/Multi_NormalEnemySpawner.cs
@@ -15,7 +15,6 @@ public class NormalMonsterSpawner
     }
 }
 
-// 이게 실시간으로 바뀌면 어쩔 수 없는데, 스테이지 들어간 후에 낙장불입이면 그냥 스테이지 시작할 때만 RPC 보내는게 좋을 듯.
 public class EnemySpawnNumManager : MonoBehaviourPun
 {
     byte[] _spawnEnemyNums = new byte[2] { 255, 255 };
@@ -64,6 +63,7 @@ public class MonsterSpawnerContorller : MonoBehaviour
     }
 
     Multi_NormalEnemy SpawnMonsterToOther(byte num, int id, int stage) => SpawnNormalMonster(num, (byte)(id == 0 ? 1 : 0), stage);
+    Multi_NormalEnemy SpawnMonsterToOther(int id, int stage) => SpawnNormalMonster(_numManager.GetSpawnEnemyNum(id), (byte)(id == 0 ? 1 : 0), stage);
 
     Multi_NormalEnemy SpawnNormalMonster(byte num, byte id, int stage)
     {
@@ -77,10 +77,9 @@ public class MonsterSpawnerContorller : MonoBehaviour
     [SerializeField] int _stageSpawnCount = 15;
     IEnumerator Co_StageSpawn(byte id, int stage)
     {
-        byte num = _numManager.GetSpawnEnemyNum(id);
         for (int i = 0; i < _stageSpawnCount; i++)
         {
-            var enemy = SpawnMonsterToOther(num, id, stage); // num 인라인 안 한 이유는 스테이지 한 번 들어가면 못 바꾸게 할려고
+            var enemy = SpawnMonsterToOther(id, stage);
             enemy.OnDead += (died) => ResurrectionMonsterToOther(enemy);
             yield return new WaitForSeconds(_spawnDelayTime);
         }
@@ -89,7 +88,7 @@ public class MonsterSpawnerContorller : MonoBehaviour
     void ResurrectionMonsterToOther(Multi_NormalEnemy enemy)
     {
         if (enemy.IsResurrection) return;
-        var spawnEnemy = SpawnMonsterToOther(_numManager.GetSpawnEnemyNum(enemy.UsingId), enemy.UsingId, enemy.SpawnStage);
+        var spawnEnemy = SpawnMonsterToOther(enemy.UsingId, enemy.SpawnStage);
         spawnEnemy.Resurrection();
     }
 

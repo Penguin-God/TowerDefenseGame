da766ec05f27d6cdf0b16e6fdb638d47873496eb
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2023-07-04 16:54:55 +0900

    feat: 트랩 간이 코드 작성

---
+++ Assets/0_ColorRandomDefance/1_Script/UserSkills/SlowTrap.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class SlowTrap : MonoBehaviour
+{
+    int layerMask = (1 << 10) | (1 << 11) | (1 << 12) | (1 << 13) | (1 << 8);
+
+    public void ApplySlowToEnemiesInRange(float range, float slowPercent, float slowTime)
+    {
+        // 덫의 위치에서 일정 반경 내의 적을 감지합니다.
+        IEnumerable<Multi_NormalEnemy> enemiesInRange = Physics.OverlapSphere(transform.position, range, layerMask).Select(x => x.GetComponent<Multi_NormalEnemy>());
+
+        foreach (var enemy in enemiesInRange)
+            enemy?.OnSlow(slowPercent, slowTime);
+    }
+}

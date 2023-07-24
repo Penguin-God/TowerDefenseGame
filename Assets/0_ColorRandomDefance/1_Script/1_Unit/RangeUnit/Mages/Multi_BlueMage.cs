da742c49edd8763e9c4e60b144f75a2532430874
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2023-07-24 18:18:32 +0900

    feat: 파란 법사 평타 패시브 추가

--- Assets/0_ColorRandomDefance/1_Script/1_Unit/RangeUnit/Mages/Multi_BlueMage.cs
+++ Assets/0_ColorRandomDefance/1_Script/1_Unit/RangeUnit/Mages/Multi_BlueMage.cs
@@ -11,7 +11,10 @@ public class Multi_BlueMage : Multi_Unit_Mage
     {
         var passiveStats = Managers.Data.GetUnitPassiveStats(UnitFlags);
         if(PhotonNetwork.IsMasterClient)
+        {
             gameObject.AddComponent<AreaSlowApplier>().SetInfo(passiveStats[0], passiveStats[1]);
+            OnPassiveHit += enemy => enemy.OnSlow(passiveStats[0], 0);
+        }
         freezeTime = skillStats[0];
     }
 

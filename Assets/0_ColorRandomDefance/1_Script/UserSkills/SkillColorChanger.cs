0760e7174522c632f367a7321e6353bdbe2d8132
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2023-07-31 13:31:43 +0900

    fix: 상대색깔변경 텍스트 버그 수정

--- Assets/0_ColorRandomDefance/1_Script/UserSkills/SkillColorChanger.cs
+++ Assets/0_ColorRandomDefance/1_Script/UserSkills/SkillColorChanger.cs
@@ -16,7 +16,7 @@ public class SkillColorChanger : MonoBehaviourPun
         var target = MultiServiceMidiator.Server.GetUnits(targetID).Where(x => x.UnitClass == targetClass).FirstOrDefault();
         if (target == null)
         {
-            ShowFaildText(targetID);
+            RPCFaildText(targetID);
             return;
         }
 
@@ -24,26 +24,25 @@ public class SkillColorChanger : MonoBehaviourPun
         ShowColorChageResultText(targetID, target.UnitFlags, resultFlag);
     }
 
-    // 인자로 넘겨준건 스킬을 적용시킬 타겟 ID라서 텍스트 띄우는 건 반대로 생각해야 됨
-    void ShowFaildText(byte targetID)
-    {
-        if (targetID == PlayerIdManager.MasterId)
-            ShowFaildText();
-        else
-            photonView.RPC(nameof(ShowFaildText), RpcTarget.Others);
-    }
-
     void ShowColorChageResultText(byte targetID, UnitFlags before, UnitFlags after)
     {
-        (string textToShowOnMaster, string textToShowOnClinet) showTexts = 
+        (string textToShowOnMaster, string textToShowOnClinet) showTexts =
             (targetID == 0) ? (_textPresenter.GenerateTextShowToVictim(before, after), _textPresenter.GenerateTextShowToDisruptor(before, after))
             : (_textPresenter.GenerateTextShowToDisruptor(before, after), _textPresenter.GenerateTextShowToVictim(before, after));
         PopupText(showTexts.textToShowOnMaster);
         photonView.RPC(nameof(PopupText), RpcTarget.Others, showTexts.textToShowOnClinet);
     }
 
-    [PunRPC] // UI 클릭할 때마다 RPC 호출이 맞는 걸까? 벗 이걸 막으려면 클라이언트에서도 Unit보유 숫자 정도는 알고 있어야 함.
-    void ShowFaildText() => PopupText(_textPresenter.ChangeFaildText);
+    // 인자로 넘겨준건 스킬을 적용시킬 타겟 ID라서 텍스트 띄우는 건 반대로 생각해야 됨
+    void RPCFaildText(byte targetID)
+    {
+        if (targetID == PlayerIdManager.MasterId)
+            photonView.RPC(nameof(ShowFaildText), RpcTarget.Others);
+        else
+            ShowFaildText();
+    }
+
+    [PunRPC] void ShowFaildText() => PopupText(_textPresenter.ChangeFaildText);
 
     [PunRPC]
     void PopupText(string text)

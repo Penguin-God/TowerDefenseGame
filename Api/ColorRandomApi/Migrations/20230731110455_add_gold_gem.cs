bedd30d77bc20af3c295ddd40c0fde42e8251ff7
jangjo123 <cjkh1324@naver.com>
2023-07-31 20:15:44 +0900

    회의

---
+++ Api/ColorRandomApi/Migrations/20230731110455_add_gold_gem.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ColorRandomApi.Migrations
+{
+    public partial class add_gold_gem : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Gem",
+                table: "Players",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Gold",
+                table: "Players",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Gem",
+                table: "Players");
+
+            migrationBuilder.DropColumn(
+                name: "Gold",
+                table: "Players");
+        }
+    }
+}

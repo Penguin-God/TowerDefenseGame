2d0c87d1f7dcadba7b1472dc5bc20bf95e8ed512
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2023-08-07 21:00:39 +0900

    refactor: 투사체 경로 계산 로직 단순화

--- Assets/0_ColorRandomDefance/1_Script/1_Unit/ProjectileThrowingUnit.cs
+++ Assets/0_ColorRandomDefance/1_Script/1_Unit/ProjectileThrowingUnit.cs
@@ -10,15 +10,14 @@ public class ProjectileThrowingUnit : MonoBehaviourPun
     public void SetInfo(string weaponPath, Transform weaponThrowPoint) => projectileData = new ProjectileData(weaponPath, transform, weaponThrowPoint);
     [SerializeField] Transform _weaponThrowPoint;
 
-    public Multi_Projectile Throw(Transform target, Action<Multi_Enemy> onHit) 
-        => ThrowWithCalculator(nameof(CallThrow), CreateProjectile(projectileData.WeaponPath, onHit), target);
     public Multi_Projectile FlatThrow(Transform target, Action<Multi_Enemy> onHit) 
-        => ThrowWithCalculator(nameof(CallFlatThrow), CreateProjectile(projectileData.WeaponPath, onHit), target);
+        => ThrowWithCalculator(nameof(CallFlat_Y_Throw), CreateProjectile(projectileData.WeaponPath, onHit), target);
     public Multi_Projectile FlatThrow(string weaponPath, Transform target, Action<Multi_Enemy> onHit)
-        => ThrowWithCalculator(nameof(CallFlatThrow), CreateProjectile(weaponPath, onHit), target);
+        => ThrowWithCalculator(nameof(CallFlat_Y_Throw), CreateProjectile(weaponPath, onHit), target);
 
     Multi_Projectile ThrowWithCalculator(string rpcName, Multi_Projectile projectile, Transform target)
     {
+        print(target == null);
         photonView.RPC(rpcName, RpcTarget.All, projectile.GetComponent<PhotonView>().ViewID, target.GetComponent<PhotonView>().ViewID);
         return projectile;
     }
@@ -30,29 +29,20 @@ public class ProjectileThrowingUnit : MonoBehaviourPun
         return projectile;
     }
 
-    public Multi_Projectile Throw(Vector3 shotPath, Action<Multi_Enemy> onHit)
+    public void Throw(Multi_Projectile projectile, Vector3 shotPath)  => photonView.RPC(nameof(Multi_Throw), RpcTarget.All, projectile.GetComponent<PhotonView>().ViewID, shotPath);
+    [PunRPC] 
+    void CallFlat_Y_Throw(int projectileId, int targetId)
     {
-        var projectile = CreateProjectile(projectileData.WeaponPath, onHit);
-        photonView.RPC(nameof(Multi_Throw), RpcTarget.All, projectile.GetComponent<PhotonView>().ViewID, shotPath);
-        return projectile;
+        Vector3 path = CalculateThorwPath(Managers.Multi.GetPhotonViewComponent<Multi_NormalEnemy>(targetId));
+        path = new Vector3(path.x, 0, path.z);
+        Multi_Throw(projectileId, path);
     }
 
-    public void Throw(Multi_Projectile projectile, Vector3 shotPath) 
-        => photonView.RPC(nameof(Multi_Throw), RpcTarget.All, projectile.GetComponent<PhotonView>().ViewID, shotPath);
-
-    [PunRPC] void CallThrow(int projectileId, int targetId) => ThrowWithCalculator(projectileId, targetId, new ShotPathCalculator());
-    [PunRPC] void CallFlatThrow(int projectileId, int targetId) => ThrowWithCalculator(projectileId, targetId, new ZeroY_ShotPathCalculator());
-
-    void ThrowWithCalculator(int projectileId, int targetId, ShotPathCalculator pathCalculator)
+    Vector3 CalculateThorwPath(Multi_NormalEnemy target)
     {
-        Vector3 path;
-        Multi_Enemy target = Managers.Multi.GetPhotonViewTransfrom(targetId).GetComponent<Multi_Enemy>();
-        if (target == null) path = projectileData.Attacker.forward.normalized;
-        else if(target.enemyType == EnemyType.Tower)
-            path = pathCalculator.CalculatePath_To_StaticTarget(projectileData.Attacker.position, target.transform.position);
-        else
-            path = pathCalculator.CalculatePath_To_MoveTarget(projectileData.Attacker.position, target.transform.position, (target as Multi_NormalEnemy).Speed, target.dir);
-        Multi_Throw(projectileId, path);
+        if (target == null) return projectileData.Attacker.forward.normalized;
+        else if (target.enemyType == EnemyType.Tower) return (target.transform.position - projectileData.Attacker.position).normalized;
+        else return new ThorwPathCalculator().CalculatePath_To_MoveTarget(projectileData.Attacker.position, target.transform.position, target.Speed, target.dir);
     }
 
     [PunRPC]
@@ -65,28 +55,14 @@ public class ProjectileThrowingUnit : MonoBehaviourPun
     }
 }
 
-public class ShotPathCalculator
+public class ThorwPathCalculator
 {
-    public virtual Vector3 CalculatePath_To_StaticTarget(Vector3 shoterPos, Vector3 targetPos) => (targetPos - shoterPos).normalized;
-    
-    // target이 움직이고 있다면 가중치를 계산함
-    readonly float WEIGHT_RATE = 2f;
-    public virtual Vector3 CalculatePath_To_MoveTarget(Vector3 shoterPos, Vector3 targetPos, float speed, Vector3 moveDir) 
+    readonly float WEIGHT_RATE = 2f; // 움직이는 타겟 보간 가중치
+    public Vector3 CalculatePath_To_MoveTarget(Vector3 shoterPos, Vector3 targetPos, float speed, Vector3 moveDir)
     {
         Vector3 dir = targetPos - shoterPos;
         float enemyWeightDir = Mathf.Lerp(0, WEIGHT_RATE, Vector3.Distance(targetPos, shoterPos) * 2 / 100);
         dir += moveDir.normalized * (0.5f * speed) * enemyWeightDir;
         return dir.normalized;
     }
-}
-
-public class ZeroY_ShotPathCalculator : ShotPathCalculator
-{
-    public override Vector3 CalculatePath_To_StaticTarget(Vector3 shoterPos, Vector3 targetPos)
-        => Y_To_Flat(base.CalculatePath_To_StaticTarget(shoterPos, targetPos));
-
-    public override Vector3 CalculatePath_To_MoveTarget(Vector3 shoterPos, Vector3 targetPos, float speed, Vector3 moveDir)
-        => Y_To_Flat(base.CalculatePath_To_MoveTarget(shoterPos, targetPos, speed, moveDir));
-
-    Vector3 Y_To_Flat(Vector3 dir) => new Vector3(dir.x, 0, dir.z);
 }

bbfaed2d2a87973aec592a29a08163b8471e08f6
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2023-08-08 16:13:25 +0900

    fix: 투사체 날리 때 null crash 나는 버그 수정

--- Assets/0_ColorRandomDefance/1_Script/1_Unit/ProjectileThrowingUnit.cs
+++ Assets/0_ColorRandomDefance/1_Script/1_Unit/ProjectileThrowingUnit.cs
@@ -22,35 +22,35 @@ public class ProjectileThrowingUnit : MonoBehaviourPun
 
     Multi_Projectile CallThrow(Multi_Projectile projectile, Transform target)
     {
-        print(target == null);
-        photonView.RPC(nameof(ThrowFlatY), RpcTarget.All, projectile.GetComponent<PhotonView>().ViewID, target.GetComponent<PhotonView>().ViewID);
+        Multi_Enemy targetMonster = target == null ? null : target.GetComponent<Multi_Enemy>();
+        Throw(projectile, new ThorwPathCalculator().CalculateThorwPath_To_Monster(targetMonster, projectileData.Attacker));
         return projectile;
     }
-
-    [PunRPC]
-    void ThrowFlatY(int projectileId, int targetId)
-        => new ProjectileThrower().ThrowFlatY(FindProjectileWithId(projectileId), Managers.Multi.GetPhotonViewComponent<Multi_NormalEnemy>(targetId), projectileData.SpawnPos, projectileData.Attacker);
 
     public void Throw(Multi_Projectile projectile, Vector3 shotPath) => photonView.RPC(nameof(Multi_Throw), RpcTarget.All, projectile.GetComponent<PhotonView>().ViewID, shotPath);
     Multi_Projectile FindProjectileWithId(int id) => Managers.Multi.GetPhotonViewTransfrom(id).GetComponent<Multi_Projectile>();
     [PunRPC]
     void Multi_Throw(int projectileId, Vector3 shotPath) => new ProjectileThrower().Thorw(FindProjectileWithId(projectileId), projectileData.SpawnPos, shotPath);
+
+
+    // 최적화는 되지만 가끔씩 null 크래쉬나는 코드
+    //Multi_Projectile CallThrow(Multi_Projectile projectile, Transform target)
+    //{
+    //    photonView.RPC(nameof(ThrowFlatY), RpcTarget.All, projectile.GetComponent<PhotonView>().ViewID, target.GetComponent<PhotonView>().ViewID);
+    //    return projectile;
+    //}
+    //[PunRPC]
+    //void ThrowFlatY(int projectileId, int targetId)
+    //    => new ProjectileThrower().ThrowFlatY(FindProjectileWithId(projectileId), Managers.Multi.GetPhotonViewComponent<Multi_NormalEnemy>(targetId), projectileData.SpawnPos, projectileData.Attacker);
 }
 
 public class ProjectileThrower
 {
     public void ThrowFlatY(Multi_Projectile projectile, Multi_Enemy target, Vector3 startPos, Transform attacker)
     {
-        Vector3 path = CalculateThorwPath(target, attacker);
+        Vector3 path = new ThorwPathCalculator().CalculateThorwPath_To_Monster(target, attacker);
         path = new Vector3(path.x, 0, path.z);
         Thorw(projectile, startPos, path);
-    }
-
-    Vector3 CalculateThorwPath(Multi_Enemy target, Transform attacker)
-    {
-        if (target == null) return attacker.forward.normalized;
-        else if (target.enemyType == EnemyType.Tower) return (target.transform.position - attacker.position).normalized;
-        else return new ThorwPathCalculator().CalculatePath_To_MoveTarget(attacker.position, target.transform.position, target.GetComponent<Multi_NormalEnemy>().Speed, target.dir);
     }
 
     public void Thorw(Multi_Projectile projectile, Vector3 startPos, Vector3 shotPath)
@@ -63,8 +63,15 @@ public class ProjectileThrower
 
 public class ThorwPathCalculator
 {
+    public Vector3 CalculateThorwPath_To_Monster(Multi_Enemy target, Transform attacker)
+    {
+        if (target == null) return attacker.forward.normalized;
+        else if (target.enemyType == EnemyType.Tower) return (target.transform.position - attacker.position).normalized;
+        else return new ThorwPathCalculator().CalculatePath_To_MoveTarget(attacker.position, target.transform.position, target.GetComponent<Multi_NormalEnemy>().Speed, target.dir);
+    }
+
     readonly float WEIGHT_RATE = 2f; // 움직이는 타겟 보간 가중치
-    public Vector3 CalculatePath_To_MoveTarget(Vector3 shoterPos, Vector3 targetPos, float speed, Vector3 moveDir)
+    Vector3 CalculatePath_To_MoveTarget(Vector3 shoterPos, Vector3 targetPos, float speed, Vector3 moveDir)
     {
         Vector3 dir = targetPos - shoterPos;
         float enemyWeightDir = Mathf.Lerp(0, WEIGHT_RATE, Vector3.Distance(targetPos, shoterPos) * 2 / 100);

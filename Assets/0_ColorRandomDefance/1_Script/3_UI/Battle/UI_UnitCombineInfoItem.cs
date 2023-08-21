35d6447443a2693dc3f4a57fa213adb938a1d6ea
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2023-08-21 19:50:36 +0900

    feat: 레시피 텍스트 코드 내에서 생성하도록 구현

--- Assets/0_ColorRandomDefance/1_Script/3_UI/Battle/UI_UnitCombineInfoItem.cs
+++ Assets/0_ColorRandomDefance/1_Script/3_UI/Battle/UI_UnitCombineInfoItem.cs
@@ -11,9 +11,17 @@ public class UI_UnitCombineInfoItem : UI_Base
 
     public void SetInfo(UnitFlags flag)
     {
-        _combineRecipeText.text = Managers.Data.UnitNameDataByFlag[flag].KoearName;
+        _combineRecipeText.text = BuildRecipeText(flag);
         _combineButton.GetComponentInChildren<TextMeshProUGUI>().text = Managers.Data.UnitNameDataByFlag[flag].KoearName;
         _combineButton.onClick.RemoveAllListeners();
         _combineButton.onClick.AddListener(() => Managers.Unit.TryCombine(flag));
+    }
+
+    string BuildRecipeText(UnitFlags targetFlag)
+    {
+        List<string> result = new List<string>();
+        foreach (var combineCondition in Managers.Data.CombineConditionByUnitFalg[targetFlag].NeedCountByFlag)
+            result.Add($"{Managers.Data.UnitNameDataByFlag[combineCondition.Key].KoearName}({combineCondition.Value})");
+        return string.Join(" + ", result);
     }
 }

2933f472c95b16f08b1d8f497a00352f3c7e029f
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2023-09-12 20:29:44 +0900

    feat: 유닛 어태커 제작

--- Assets/0_ColorRandomDefance/1_Script/Contorller/Unit/Attack/SwordmanAttackActor.cs
+++ Assets/0_ColorRandomDefance/1_Script/Contorller/Unit/Attack/SwordmanAttackActor.cs
@@ -6,6 +6,11 @@ public class SwordmanAttackActor : MonoBehaviour, IUnitAttackActor
 {
     [SerializeField] GameObject _trail;
     [SerializeField] Animator animator;
+    UnitAttacker _unitAttacker;
+    public void RecevieInject(UnitAttacker unitAttacker)
+    {
+        _unitAttacker = unitAttacker;
+    }
 
     public float AttackCoolTime => 0.5f;
     public IEnumerator Do(Multi_Enemy target)
@@ -14,7 +19,7 @@ public class SwordmanAttackActor : MonoBehaviour, IUnitAttackActor
         yield return new WaitForSeconds(0.8f);
         _trail.SetActive(true);
         yield return new WaitForSeconds(0.3f);
-        // HitMeeleAttack();
+        _unitAttacker.Attack(target);
         _trail.SetActive(false);
     }
 }
---
+++ Assets/0_ColorRandomDefance/1_Script/Domain/Unit/UnitAttacker.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitAttacker
+{
+    readonly Unit _unit;
+
+    public UnitAttacker(Unit unit)
+    {
+        _unit = unit;
+    }
+
+    public void Attack(Multi_Enemy target) { }
+}

8834376c063522965f0b17a24d16d83ecbd1827d
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2023-09-13 16:49:37 +0900

    chore: 기사 변형 프리펩 생성

--- Assets/0_ColorRandomDefance/1_Script/Presenters/ResourcesPathBuilder.cs
+++ Assets/0_ColorRandomDefance/1_Script/Presenters/ResourcesPathBuilder.cs
@@ -17,7 +17,7 @@ public class ResourcesPathBuilder
 
     public string BuildUnitPath(UnitFlags flag)
     {
-        if(flag.UnitClass == UnitClass.Archer)
+        if(flag.UnitClass == UnitClass.Archer || flag.UnitClass == UnitClass.Swordman)
             return $"Unit/{GetClassName(flag.UnitClass)}/{Enum.GetName(typeof(UnitColor), flag.UnitColor)}{GetClassName(flag.UnitClass)}";
         else
             return $"Unit/{GetClassName(flag.UnitClass)}/{Enum.GetName(typeof(UnitColor), flag.UnitColor)}_{GetClassName(flag.UnitClass)} 1";

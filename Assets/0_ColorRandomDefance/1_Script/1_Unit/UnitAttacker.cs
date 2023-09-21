e9308caf5ba0bc572cd4083feb6fc5a5fb2369f0
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2023-09-21 15:11:57 +0900

    fix: 공격 예외처리

--- Assets/0_ColorRandomDefance/1_Script/1_Unit/UnitAttacker.cs
+++ Assets/0_ColorRandomDefance/1_Script/1_Unit/UnitAttacker.cs
@@ -10,9 +10,14 @@ public class UnitAttacker
     // 스킬일 때는 더 높은 대미지였나?
     public int CalculateDamage(EnemyType type) => type == EnemyType.Normal ? _unit.DamageInfo.ApplyDamage : _unit.DamageInfo.ApplyBossDamage;
 
-    public void NormalAttack(Multi_Enemy target) => Attack(target, CalculateDamage(target.enemyType), false);
-    public void SkillAttack(Multi_Enemy target) => Attack(target, CalculateDamage(target.enemyType), true);
+    public void NormalAttack(Multi_Enemy target) => Attack(target, false);
+    public void SkillAttack(Multi_Enemy target) => Attack(target, true);
     public void SkillAttack(Multi_Enemy target, int damage) => Attack(target, damage, true);
+    void Attack(Multi_Enemy target, bool isSkill)
+    {
+        if (target != null)
+            Attack(target, CalculateDamage(target.enemyType), isSkill);
+    }
     void Attack(Multi_Enemy target, int damage, bool isSkill)
     {
         if (target == null) return;

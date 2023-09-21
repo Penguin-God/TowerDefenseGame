56a83a9cc626afe9d75aac7b64417614c8f0e96c
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2023-09-21 16:22:38 +0900

    chore: 폴더 정리

--- Assets/0_ColorRandomDefance/1_Script/1_Unit/RangeUnit/Mages/Multi_OrangeMage.cs
+++ Assets/0_ColorRandomDefance/1_Script/1_Unit/RangeUnit/Mages/Multi_OrangeMage.cs
@@ -14,7 +14,6 @@ public class Multi_OrangeMage : Multi_Unit_Mage
         percent = skillStats[1];
     }
 
-    // TODO : 법사 스킬 중에 기본 공격 허용할건지 물어보기
     protected override void MageSkile()
         => SkillSpawn(Vector3.zero).GetComponent<Multi_OrangeSkill>().OnSkile(TargetEnemy, BossDamage, count, percent);
 }

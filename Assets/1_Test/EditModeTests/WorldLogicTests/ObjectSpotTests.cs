e9bc651a4b882255c70c128af432b7f023abf026
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2023-09-24 17:04:52 +0900

    test: 동치성 테스트 강화

--- Assets/1_Test/EditModeTests/WorldLogicTests/ObjectSpotTests.cs
+++ Assets/1_Test/EditModeTests/WorldLogicTests/ObjectSpotTests.cs
@@ -21,13 +21,15 @@ namespace WorldLogicTests
         }
 
         [Test]
-        [TestCase(0, true)]
-        [TestCase(0, false)]
-        [TestCase(1, true)]
-        [TestCase(1, false)]
-        public void 같은_값을_들고_있으면_동일한_것으로_판단해야_함(byte startId, bool startWorld)
+        [TestCase(0, true, 0, true, true)]
+        [TestCase(1, false, 1, false, true)]
+        [TestCase(0, true, 1, true, false)]
+        [TestCase(1, false, 1, true, false)]
+        [TestCase(0, false, 1, true, false)]
+        public void 같은_값을_들고_있으면_동일한_것으로_판단해야_함(byte startId1, bool startWorld1, byte startId2, bool startWorld2, bool expected)
         {
-            Assert.AreEqual(CreateSpot(startId, startWorld), CreateSpot(startId, startWorld));
+            bool result = CreateSpot(startId1, startWorld1) == CreateSpot(startId2, startWorld2);
+            Assert.AreEqual(expected, result);
         }
     }
 }

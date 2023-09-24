ddd199f7201ca9d1ff502ec8a48c3cbe0abb2a9b
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2023-09-24 16:52:14 +0900

    test: 동치성 검사 추가

--- Assets/0_ColorRandomDefance/1_Script/Domain/ObjectSpot.cs
+++ Assets/0_ColorRandomDefance/1_Script/Domain/ObjectSpot.cs
@@ -4,14 +4,27 @@ using System.Collections.Generic;
 public struct ObjectSpot
 {
     public readonly byte WorldId;
-    public readonly bool IsInDefenseWolrd;
+    public readonly bool IsInDefenseWorld;
 
     public ObjectSpot(byte worldId, bool isInDefenseWolrd)
     {
         WorldId = worldId;
-        IsInDefenseWolrd = isInDefenseWolrd;
+        IsInDefenseWorld = isInDefenseWolrd;
     }
 
-    public ObjectSpot ChangeWorldId() => new ObjectSpot((byte)(WorldId == 0 ? 1 : 0), IsInDefenseWolrd);
-    public ObjectSpot ChangeWorldType() => new ObjectSpot(WorldId, !IsInDefenseWolrd);
+    public ObjectSpot ChangeWorldId() => new ObjectSpot((byte)(WorldId == 0 ? 1 : 0), IsInDefenseWorld);
+    public ObjectSpot ChangeWorldType() => new ObjectSpot(WorldId, !IsInDefenseWorld);
+
+    public static bool operator ==(ObjectSpot a, ObjectSpot b) => a.WorldId == b.WorldId && a.IsInDefenseWorld == b.IsInDefenseWorld;
+
+    public static bool operator !=(ObjectSpot a, ObjectSpot b) => !(a == b);
+
+    public override bool Equals(object obj)
+    {
+        if (obj is ObjectSpot other)
+            return this == other;
+        return false;
+    }
+
+    public override int GetHashCode() => WorldId.GetHashCode() ^ IsInDefenseWorld.GetHashCode();
 }
--- Assets/1_Test/EditModeTests/WorldLogicTests/ObjectSpotTests.cs
+++ Assets/1_Test/EditModeTests/WorldLogicTests/ObjectSpotTests.cs
@@ -8,14 +8,26 @@ namespace WorldLogicTests
 {
     public class ObjectSpotTests
     {
+        ObjectSpot CreateSpot(byte id, bool isInDefenseWorld) => new ObjectSpot(id, isInDefenseWorld);
+
         [Test]
         [TestCase(0, 1)]
         [TestCase(1, 0)]
         public void 월드_변경_시_아이디가_반대쪽으로_바뀌어야_함(byte startId, byte expected)
         {
-            var spot = new ObjectSpot(startId, true);
+            var spot = CreateSpot(startId, true);
 
             Assert.AreEqual(expected, spot.ChangeWorldId().WorldId);
+        }
+
+        [Test]
+        [TestCase(0, true)]
+        [TestCase(0, false)]
+        [TestCase(1, true)]
+        [TestCase(1, false)]
+        public void 같은_값을_들고_있으면_동일한_것으로_판단해야_함(byte startId, bool startWorld)
+        {
+            Assert.AreEqual(CreateSpot(startId, startWorld), CreateSpot(startId, startWorld));
         }
     }
 }

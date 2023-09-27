1a47637742e78772c28690009cac0b6c38c33590
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2023-09-27 17:49:31 +0900

    test: 방향 계산 유틸 테스트 작성 및 통과

--- Assets/0_ColorRandomDefance/1_Script/Domain/Util/MathUtill.cs
+++ Assets/0_ColorRandomDefance/1_Script/Domain/Util/MathUtill.cs
@@ -4,6 +4,37 @@ using UnityEngine;
 public static class MathUtil
 {
     public static float CalculatePercentage(float total, float percent) => (total * percent) / 100f;
-
     public static int CalculatePercentage(int total, float percent) => Mathf.RoundToInt(CalculatePercentage((float)total, percent));
+
+    public static Vector2[] CalculateDirections(int dirCount)
+    {
+        Vector2[] directions = new Vector2[dirCount];
+        float angleStep = 360f / dirCount;
+
+        for (int i = 0; i < dirCount; i++)
+        {
+            float angle = i * angleStep;
+            directions[i] = new Vector2(Mathf.Cos(angle * Mathf.Deg2Rad), Mathf.Sin(angle * Mathf.Deg2Rad));
+        }
+
+        return directions;
+    }
+
+    public static Vector2[] CalculateCirclePositions(int positionCount, float radius)
+    {
+        Vector2[] startingPositions = new Vector2[positionCount];
+        float angleStep = 360f / positionCount;
+
+        for (int i = 0; i < positionCount; i++)
+        {
+            float angle = i * angleStep;
+            float x = Mathf.Cos(angle * Mathf.Deg2Rad) * radius;
+            float y = Mathf.Sin(angle * Mathf.Deg2Rad) * radius;
+            startingPositions[i] = new Vector2(x, y);
+        }
+
+        return startingPositions;
+    }
+
+
 }
--- Assets/1_Test/EditModeTests/UtilDomainTests/MathTests.cs
+++ Assets/1_Test/EditModeTests/UtilDomainTests/MathTests.cs
@@ -25,4 +25,65 @@ public class MathTests
     {
         Assert.AreEqual(expected, MathUtil.CalculatePercentage(total, percent));
     }
+
+    [Test]
+    public void 방향_8개_계산_시_원을_기준으로_동일한_간격만큼_벌어져야_함()
+    {
+        // Arrange
+        int numberOfDirections = 8;
+        float delta = 0.01f; // 허용 가능한 오차
+
+        Vector2[] expectedDirections =
+        {
+            new Vector2(1f, 0f), // 0도
+            new Vector2(0.7071f, 0.7071f), // 45도
+            new Vector2(0f, 1f), // 90도
+            new Vector2(-0.7071f, 0.7071f), // 135도
+            new Vector2(-1f, 0f), // 180도
+            new Vector2(-0.7071f, -0.7071f), // 225도
+            new Vector2(0f, -1f), // 270도
+            new Vector2(0.7071f, -0.7071f) // 315도
+        };
+
+        // Act
+        Vector2[] actualDirections = MathUtil.CalculateDirections(numberOfDirections);
+
+        // Assert
+        for (int i = 0; i < numberOfDirections; i++)
+        {
+            Assert.That(expectedDirections[i].x, Is.EqualTo(actualDirections[i].x).Within(delta));
+            Assert.That(expectedDirections[i].y, Is.EqualTo(actualDirections[i].y).Within(delta));
+        }
+    }
+
+    [Test]
+    public void 위치_8개_계산_시_중심에서_반지름만큼_떨어지고_위치끼리_동일한_간격만큼_벌어져야_함()
+    {
+        // Arrange
+        int numberOfPositions = 8;
+        float distanceFromCenter = 1f; // 단위 원을 기준으로 함
+        float delta = 0.01f; // 허용 가능한 오차
+
+        Vector2[] expectedPositions =
+        {
+            new Vector2(1f, 0f), // 0도
+            new Vector2(0.7071f, 0.7071f), // 45도
+            new Vector2(0f, 1f), // 90도
+            new Vector2(-0.7071f, 0.7071f), // 135도
+            new Vector2(-1f, 0f), // 180도
+            new Vector2(-0.7071f, -0.7071f), // 225도
+            new Vector2(0f, -1f), // 270도
+            new Vector2(0.7071f, -0.7071f) // 315도
+        };
+
+        // Act
+        Vector2[] actualPositions = MathUtil.CalculateCirclePositions(numberOfPositions, distanceFromCenter);
+
+        // Assert
+        for (int i = 0; i < numberOfPositions; i++)
+        {
+            Assert.That(expectedPositions[i].x, Is.EqualTo(actualPositions[i].x).Within(delta));
+            Assert.That(expectedPositions[i].y, Is.EqualTo(actualPositions[i].y).Within(delta));
+        }
+    }
 }

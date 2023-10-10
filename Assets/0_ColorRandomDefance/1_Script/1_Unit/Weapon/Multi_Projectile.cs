b1e8dca32c435eb3db3090962b1c6a678427b2fa
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2023-10-10 13:47:15 +0900

    refactor: 투사체랑 자동 파괴 로직 분리

--- Assets/0_ColorRandomDefance/1_Script/1_Unit/Weapon/Multi_Projectile.cs
+++ Assets/0_ColorRandomDefance/1_Script/1_Unit/Weapon/Multi_Projectile.cs
@@ -7,19 +7,12 @@ using Photon.Pun;
 public class Multi_Projectile : MonoBehaviour
 {
     [SerializeField] bool isAOE; // area of effect : 범위(광역) 공격
-    [SerializeField] float aliveTime = 5f;
     [SerializeField] protected int _speed;
     protected Rigidbody Rigidbody = null;
     protected Action<Multi_Enemy> OnHit = null;
-
     void Awake()
     {
         Rigidbody = GetComponent<Rigidbody>();
-    }
-
-    void OnEnable()
-    {
-        StartCoroutine(Co_Inactive(aliveTime));
     }
 
     protected void Shot(Vector3 dir)
@@ -35,31 +28,20 @@ public class Multi_Projectile : MonoBehaviour
         Shot(dir);
     }
 
-    IEnumerator Co_Inactive(float delayTime)
-    {
-        yield return new WaitForSeconds(delayTime);
-        ReturnObjet();
-    }
-
-    void ReturnObjet()
-    {
-        OnHit = null;
-        StopAllCoroutines();
-        Managers.Resources.Destroy(gameObject);
-    }
-
-    protected virtual void OnTriggerHit(Collider other) 
+    protected virtual void OnTriggerHit(Collider other)
     {
         if (other.transform.parent == null) return;
 
         // 컴포넌트가 부모에게 있음
         if (other.transform.parent.TryGetComponent<Multi_Enemy>(out var enemy) == false) return;
         if (PhotonNetwork.IsMasterClient) OnHit?.Invoke(enemy);
-        if (isAOE == false) ReturnObjet();
+        if (isAOE == false) GetComponent<AutoDestoryAfterSecond>().ReturnObjet();
     }
 
     void OnTriggerEnter(Collider other)
     {
         OnTriggerHit(other);
     }
+
+    void OnDisable() => OnHit = null;
 }

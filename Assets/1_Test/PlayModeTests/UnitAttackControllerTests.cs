7f9b4ef985d211be1c5d50e85a950377c3eaa571
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2023-10-17 14:01:31 +0900

    test: 유닛 공격 및 공속 테스트 작성 및 통과

---
+++ Assets/1_Test/PlayModeTests/UnitAttackControllerTests.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class UnitAttackControllerTests
+{
+    UnitAttackState CreateAttackState(bool isAttackable, bool isAttack) => new UnitAttackState(isAttackable, isAttack);
+
+    [UnityTest]
+    public IEnumerator 유닛_공격은_정해진_시간에_따라_상태가_바뀌어야_함()
+    {
+        yield return 유닛_공격은_공속에_따라_상태가_바뀌어야_함(1, 0.02f, 0.021f, 0.021f);
+        yield return 유닛_공격은_공속에_따라_상태가_바뀌어야_함(2, 0.02f, 0.011f, 0.011f);
+    }
+
+    public IEnumerator 유닛_공격은_공속에_따라_상태가_바뀌어야_함(float _attackSpeed, float coolDown, float firstDelay, float secondDelay)
+    {
+        var sut = new GameObject("Attacker").AddComponent<TestAttacker>();
+        var stateManager = new UnitStateManager(new ObjectSpot());
+        sut.DependencyInject(stateManager, new Unit(new UnitFlags(), new UnitStats(new UnitDamageInfo(), 0, attackSpeed: _attackSpeed, 0, 0)));
+        sut.DoAttack(coolDown);
+        Assert.AreEqual(stateManager.UnitAttackState, CreateAttackState(false, true));
+        yield return new WaitForSeconds(firstDelay);
+        Assert.AreEqual(stateManager.UnitAttackState, CreateAttackState(false, false));
+        yield return new WaitForSeconds(secondDelay);
+        Assert.AreEqual(stateManager.UnitAttackState, CreateAttackState(true, false));
+    }
+}
+
+public class TestAttacker : UnitAttackControllerTemplate
+{
+    protected override IEnumerator Co_Attack()
+    {
+        yield return WaitSecond(0.01f);
+    }
+}

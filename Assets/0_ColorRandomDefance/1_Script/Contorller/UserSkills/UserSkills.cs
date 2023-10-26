ef052a2c65c08f7f3dd292d887156f6876610942
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2023-10-26 17:42:04 +0900

    refactor: 마나변이 로직 간소화

--- Assets/0_ColorRandomDefance/1_Script/Contorller/UserSkills/UserSkills.cs
+++ Assets/0_ColorRandomDefance/1_Script/Contorller/UserSkills/UserSkills.cs
@@ -142,7 +142,7 @@ public class UserSkillActor
         {
             case SkillType.태극스킬: result = new TaegeukController(skillBattleData, container.GetComponent<MultiUnitStatController>()); break;
             case SkillType.흑의결속: result = new BlackUnitUpgrade(skillBattleData, container.GetComponent<MultiUnitStatController>()); break;
-            case SkillType.마나변이: result = new ManaMutation(skillBattleData, container.GetComponent<SkillColorChanger>()); break;
+            case SkillType.마나변이: result = new ManaMutation(skillBattleData, container.GetComponent<SkillColorChanger>(), container.GetEventDispatcher()); break;
             case SkillType.마나불능: result = new ManaImpotence(skillBattleData); break;
             case SkillType.장사꾼: result = new UnitMerchant(skillBattleData); break;
             case SkillType.도박사: 
@@ -212,30 +212,19 @@ public class BlackUnitUpgrade : UserSkill
     }
 }
 
-public class ManaMutation : UserSkill // 하얀 유닛을 뽑을 때 뽑은 직업과 같은 상대 유닛의 색깔을 다른 색깔로 변경
+public class ManaMutation : UserSkill
 {
-    int[] _whiteUnitCounts = new int[4];
-    public event Action<byte, byte> OnUnitColorChanaged; // 변하기 전 색깔, 변한 후 색깔
-    readonly SkillColorChanger _colorChanger;
-
-    public ManaMutation(UserSkillBattleData userSkillBattleData, SkillColorChanger colorChanger) : base(userSkillBattleData)
-        => _colorChanger = colorChanger;
-
-    internal override void InitSkill()
+    public ManaMutation(UserSkillBattleData userSkillBattleData, SkillColorChanger colorChanger, BattleEventDispatcher dispatcher) : base(userSkillBattleData)
     {
-        Managers.Unit.OnUnitCountChangeByFlag += UseSkill;
+        dispatcher.OnUnitSpawn += UseSkill;
+
+        // 하얀 유닛을 뽑을 때 뽑은 직업과 같은 상대 유닛의 색깔을 다른 색깔로 변경
+        void UseSkill(Multi_TeamSoldier unit)
+        {
+            if (unit.UnitColor == UnitColor.White)
+                colorChanger.ColorChangeSkill(unit.UnitClass);
+        }
     }
-
-    void UseSkill(UnitFlags flag, int newCount)
-    {
-        if (flag.UnitColor != UnitColor.White) return;
-
-        if (UnitCountIncreased(flag, newCount))
-            _colorChanger.ColorChangeSkill(flag.UnitClass);
-        _whiteUnitCounts[flag.ClassNumber] = newCount;
-    }
-
-    bool UnitCountIncreased(UnitFlags flag, int newCount) => newCount > _whiteUnitCounts[flag.ClassNumber];
 }
 
 public class ManaImpotence : UserSkill

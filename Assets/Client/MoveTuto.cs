4ba19e809523f3dbf9f8fbdcf1d608e6993b3395
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2023-11-16 13:01:23 +0900

    refactor: 쓸모없는거 제거

--- Assets/Client/MoveTuto.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class MoveTuto : MonoBehaviour
-{
-    public void Tuto() => Managers.Scene.LoadScene(SceneTyep.Tutorial);
-}

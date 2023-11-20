57bd53242fd22dcdadcbefc1a3f16dabfd286b50
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2023-11-20 14:57:14 +0900

    feat: 유닛 강화 동작시킴

--- Assets/0_ColorRandomDefance/1_Script/3_UI/Battle/Shop UI/UI_UnitUpgradeIcon.cs
+++ Assets/0_ColorRandomDefance/1_Script/3_UI/Battle/Shop UI/UI_UnitUpgradeIcon.cs
@@ -2,29 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UI_UnitUpgradeIcon : UI_Base
 {
     UI_UnitIcon _unitIcon;
     TextMeshProUGUI _levelText;
+    Button _upgradeButton;
     int _upgradeLevel = 1;
     int MaxLevel;
     protected override void Init()
     {
         _unitIcon = GetComponent<UI_UnitIcon>();
         _levelText = GetComponentInChildren<TextMeshProUGUI>();
+        _upgradeButton = GetComponentInChildren<Button>();
     }
 
     public void FillGoods(UnitColor color, UnitUpgradeData upgradeData, MultiUnitStatController statController, int maxLevel)
     {
         CheckInit();
         _unitIcon.SetBGColor(color);
-        _unitIcon.BindClickEvent(UpgradeUnit);
+        _upgradeButton.onClick.AddListener(UpgradeUnit);
         MaxLevel = maxLevel;
         UpdateLevelText();
 
         void UpgradeUnit()
         {
+            if (IsMaxUpgrade()) return;
+
             if (Multi_GameManager.Instance.TryUseGold(5))
             {
                 const float Percentage = 100f;
@@ -33,6 +38,7 @@ public class UI_UnitUpgradeIcon : UI_Base
                     case UnitUpgradeType.Value: statController.AddUnitDamage(upgradeData.TargetColor, upgradeData.Value, UnitStatType.All); break;
                     case UnitUpgradeType.Scale: statController.ScaleUnitDamage(upgradeData.TargetColor, upgradeData.Value / Percentage, UnitStatType.All); break;
                 }
+                _upgradeLevel++;
                 UpdateLevelText();
             }
         }
@@ -40,9 +46,10 @@ public class UI_UnitUpgradeIcon : UI_Base
 
     void UpdateLevelText()
     {
-        if( _upgradeLevel >= MaxLevel )
-            _levelText.text = "Level : MAX";
+        if(IsMaxUpgrade())
+            _levelText.text = "LV : MAX";
         else
-            _levelText.text = $"Level : {_upgradeLevel}";
+            _levelText.text = $"LV : {_upgradeLevel}";
     }
+    bool IsMaxUpgrade() => _upgradeLevel >= MaxLevel;
 }

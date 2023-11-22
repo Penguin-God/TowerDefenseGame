56e3839001e717c54798ee2ccb0e00835476cecf
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2023-11-22 19:17:57 +0900

    test: 스킬 뽑기 테스트 작성 및 통과

---
+++ Assets/0_ColorRandomDefance/1_Script/Domain/Lobby/SkillDrawer.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Random = UnityEngine.Random;
+
+public readonly struct SkillDrawInfo
+{
+    public readonly UserSkillClass SkillClass;
+    public readonly int MinCount;
+    public readonly int MaxCount;
+
+    public SkillDrawInfo(UserSkillClass skillClass, int minCount, int maxCount)
+    {
+        if(maxCount <= 0)
+            throw new ArgumentException($"최소가 0보다 작거나 같음. minCount : {minCount}");
+        if (minCount > maxCount)
+            throw new ArgumentException($"최소 개수가 최대 개수보다 큼. minCount : {minCount}, maxCount : {maxCount}");
+        SkillClass = skillClass;
+        MinCount = minCount;
+        MaxCount = maxCount;
+    }
+}
+
+public readonly struct SkillDrawResultInfo
+{
+    public readonly SkillType SkillType;
+    public readonly int Amount;
+
+    public SkillDrawResultInfo(SkillType skillType, int amount)
+    {
+        SkillType = skillType;
+        Amount = amount;
+    }
+}
+
+public class SkillDrawer
+{
+    readonly IReadOnlyDictionary<UserSkillClass, IReadOnlyList<SkillType>> _skillByClass;
+    public SkillDrawer(IReadOnlyDictionary<UserSkillClass, IReadOnlyList<SkillType>> skillByClass) => _skillByClass = skillByClass;
+
+    public IEnumerable<SkillDrawResultInfo> DrawSkills(IEnumerable<SkillDrawInfo> drawInfos)
+    {
+        List<SkillDrawResultInfo> result = new();
+        foreach (var info in drawInfos)
+        {
+            IReadOnlyList<SkillType> drawableSkills = _skillByClass[info.SkillClass].Except(result.Select(x => x.SkillType)).ToList();
+            SkillType drawSkill = drawableSkills[Random.Range(0, drawableSkills.Count)];
+            int drawAmount = Random.Range(info.MinCount, info.MaxCount + 1);
+            result.Add(new SkillDrawResultInfo(drawSkill, drawAmount));
+        }
+        return result;
+    }
+}
---
+++ Assets/1_Test/EditModeTests/SkillDrawerTests.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class SkillDrawerTests
+{
+    [Test]
+    public void 뽑은_스킬과_수량은_등급과_범위_내에서_나와야_함()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            AssertSkillDraw();
+        }
+    }
+
+    void AssertSkillDraw()
+    {
+        // Arrange
+        var skillDatas = new Dictionary<UserSkillClass, IReadOnlyList<SkillType>>()
+        {
+            {  UserSkillClass.Main, new SkillType[]{ SkillType.흑의결속, SkillType.태극스킬} },
+            {  UserSkillClass.Sub, new SkillType[]{ SkillType.시작골드증가, SkillType.거인학살자} }
+        };
+        SkillDrawer drawer = new SkillDrawer(skillDatas);
+        var drawInfos = new List<SkillDrawInfo>
+        {
+            new SkillDrawInfo(UserSkillClass.Main, 1, 10),
+            new SkillDrawInfo(UserSkillClass.Main, 20, 40),
+            new SkillDrawInfo(UserSkillClass.Sub, 30, 80),
+        };
+
+        // Act
+        var drawnSkills = drawer.DrawSkills(drawInfos).ToArray();
+
+        // Assert
+        Assert.AreEqual(3, drawnSkills.Select(x => x.SkillType).Distinct().Count());
+        CollectionAssert.Contains(skillDatas[UserSkillClass.Main], drawnSkills[0].SkillType);
+        CollectionAssert.Contains(skillDatas[UserSkillClass.Main], drawnSkills[1].SkillType);
+        CollectionAssert.Contains(skillDatas[UserSkillClass.Sub], drawnSkills[2].SkillType);
+
+        Assert.That(drawnSkills[0].Amount, Is.InRange(1, 10));
+        Assert.That(drawnSkills[1].Amount, Is.InRange(20, 40));
+        Assert.That(drawnSkills[2].Amount, Is.InRange(30, 80));
+    }
+}

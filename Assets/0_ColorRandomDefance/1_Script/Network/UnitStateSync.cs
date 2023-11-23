1c1bfaaa1ef721f21689e787329f08b234b322e5
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2023-11-23 20:53:17 +0900

    feat: 보간 속도, 범위 조정

--- Assets/0_ColorRandomDefance/1_Script/Network/UnitStateSync.cs
+++ Assets/0_ColorRandomDefance/1_Script/Network/UnitStateSync.cs
@@ -2,6 +2,7 @@ using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class UnitStateSync : MonoBehaviourPun, IPunObservable
 {
@@ -10,6 +11,7 @@ public class UnitStateSync : MonoBehaviourPun, IPunObservable
         photonView.ObservedComponents.Add(this);
         _unitChaseSystem = GetComponent<UnitChaseSystem>();
         _unit = GetComponent<Multi_TeamSoldier>();
+        _agent = GetComponent<NavMeshAgent>();
     }
 
     void OnEnable()
@@ -20,6 +22,7 @@ public class UnitStateSync : MonoBehaviourPun, IPunObservable
 
     UnitChaseSystem _unitChaseSystem;
     Multi_TeamSoldier _unit;
+    NavMeshAgent _agent;
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
         if (stream.IsWriting)
@@ -41,9 +44,10 @@ public class UnitStateSync : MonoBehaviourPun, IPunObservable
         }
     }
 
-    const float RotationLerpSpeed = 5.0f;
-    const float PositionLerpSpeed = 10f;
-    const float Delta = 10f;
+    const float RotationLerpSpeed = 0.8f;
+    const float PositionLerpSpeed = 0.4f;
+    const float AngleDelta = 5f;
+    const float PositionDelta = 5f;
     float _masterRotationY;
     Vector3 _masterPos;
     int _targetId = -1;
@@ -53,14 +57,24 @@ public class UnitStateSync : MonoBehaviourPun, IPunObservable
 
         // 마스터 클라이언트로부터 받은 회전값 로컬 회전값을 비교 및 보간
         float currentRotationY = transform.eulerAngles.y;
-        if (Mathf.Abs(_masterRotationY - currentRotationY) > Delta)
+        if (Mathf.Abs(_masterRotationY - currentRotationY) > AngleDelta)
         {
+            print("방향 보간");
             float newY = Mathf.LerpAngle(currentRotationY, _masterRotationY, Time.deltaTime * RotationLerpSpeed);
             transform.eulerAngles = new Vector3(transform.eulerAngles.x, newY, transform.eulerAngles.z);
         }
 
-        if (Vector3.Distance(transform.position, _masterPos) > 2)
+        if (Vector3.Distance(transform.position, _masterPos) > PositionDelta)
             transform.position = Vector3.Lerp(transform.position, _masterPos, Time.deltaTime * PositionLerpSpeed);
+
+        if (Vector3.Distance(transform.position, _masterPos) > PositionDelta)
+            print("위치 보간");
+
+        // 이거 사용하려면 유닛에서 isStopped 건드리는 부분 고려해야 함.
+        //if (Vector3.Distance(transform.position, _masterPos) > PositionDelta)
+        //    _agent.isStopped = true;
+        //else
+        //    _agent.isStopped = false;
 
         if (_targetId > 0) _unit.ChangeTarget(_targetId);
         else _unit.SetNull();

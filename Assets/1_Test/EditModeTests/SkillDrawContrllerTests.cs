05f200c5485cec4969d327b819faacc37b98fe91
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2023-11-24 13:46:08 +0900

    test: 테스트 강화

--- Assets/1_Test/EditModeTests/SkillDrawContrllerTests.cs
+++ Assets/1_Test/EditModeTests/SkillDrawContrllerTests.cs
@@ -10,22 +10,33 @@ public class SkillDrawContrllerTests
     [Test]
     public void 뽑기를_하면_유저_데이터가_갱신되고_영속성_저장도_해야_됨()
     {
+        // Arrange
         PlayerDataManager playerDataManager = new(new SkillInventroy(new Dictionary<SkillType, PlayerOwnedSkillInfo>()));
         TestPersistence persistence = new();
-        SkillDrawer skillDrawer = new(new Dictionary<UserSkillClass, IReadOnlyList<SkillType>>() { { UserSkillClass.Main, new SkillType[] { SkillType.흑의결속 } } });
-        var sut = new SkillDrawUseCase(skillDrawer, playerDataManager, persistence);
-
+        var skillDatas = new Dictionary<UserSkillClass, IReadOnlyList<SkillType>>()
+        {
+            {  UserSkillClass.Main, new SkillType[]{ SkillType.흑의결속, SkillType.태극스킬} },
+            {  UserSkillClass.Sub, new SkillType[]{ SkillType.시작골드증가, SkillType.거인학살자} }
+        };
+        SkillDrawer skillDrawer = new SkillDrawer(skillDatas);
         var drawInfos = new List<SkillDrawInfo>
         {
             new SkillDrawInfo(UserSkillClass.Main, 1, 10),
+            new SkillDrawInfo(UserSkillClass.Main, 20, 40),
+            new SkillDrawInfo(UserSkillClass.Sub, 30, 80),
         };
-        var result = sut.DrawSkills(drawInfos).FirstOrDefault();
 
+        // Act
+        var sut = new SkillDrawUseCase(skillDrawer, playerDataManager, persistence);
+
+        // Assert
+        foreach (var resultInfo in sut.DrawSkills(drawInfos))
+        {
+            Assert.IsTrue(playerDataManager.SkillInventroy.HasSkill(resultInfo.SkillType));
+            var skillData = playerDataManager.SkillInventroy.GetSkillInfo(resultInfo.SkillType);
+            Assert.AreEqual(skillData.HasAmount, resultInfo.Amount);
+        }
         
-        Assert.IsTrue(playerDataManager.SkillInventroy.HasSkill(result.SkillType));
-        var skillData = playerDataManager.SkillInventroy.GetSkillInfo(result.SkillType);
-        Assert.AreEqual(skillData.HasAmount, result.Amount);
-
         // 영속성 저장은 실행되었는지만 체크
         Assert.IsTrue(persistence.IsExecute);
     }

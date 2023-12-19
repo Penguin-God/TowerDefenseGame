c4f1e6ef0c579800b457ff7124346d542b4bf380
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2023-12-19 16:21:21 +0900

    feat: 스킬 스탯 텍스트 로드

--- Assets/0_ColorRandomDefance/1_Script/Lobby/SkillInfoPresenter.cs
+++ Assets/0_ColorRandomDefance/1_Script/Lobby/SkillInfoPresenter.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
 public class SkillInfoPresenter
@@ -14,9 +13,10 @@ public class SkillInfoPresenter
         _skillDataGetter = skillDataGetter;
     }
 
-    public string GetSkillName() => Managers.Data.UserSkill.GetSkillGoodsData(SkillType).SkillName;
-    public string GetSkillDescription() => Managers.Data.UserSkill.GetSkillGoodsData(SkillType).Description;
-    public UserSkillClass GetSkillClass() => Managers.Data.UserSkill.GetSkillGoodsData(SkillType).SkillClass;
+    UserSkillGoodsData GetSkillData() => Managers.Data.UserSkill.GetSkillGoodsData(SkillType);
+    public string GetSkillName() => GetSkillData().SkillName;
+    public string GetSkillDescription() => GetSkillData().Description;
+    public UserSkillClass GetSkillClass() => GetSkillData().SkillClass;
     public Sprite GetSkillImage() => SpriteUtility.GetSkillImage(SkillType);
     public int GetSkillLevel() => _skillDataGetter.GetSkillLevel(SkillType);
     public int GetGoldForUpgrade() => _skillDataGetter.GetSkillUpgradeData(SkillType).NeedGold;
@@ -24,6 +24,26 @@ public class SkillInfoPresenter
     public float GetExpGaugeAmount() => (float)_skillDataGetter.GetSkillExp(SkillType) / _skillDataGetter.GetNeedLevelUpExp(SkillType);
     public IEnumerable<string> GetSkillStatTexts()
     {
-        return new string[] { "2", "4", "5", "6" };
+        var result = new List<string>();
+        IReadOnlyList<string> statTexts = GetSkillData().StatInfoFraems;
+        for (int i = 0; i < statTexts.Count; i++)
+        {
+            string text = statTexts[i].Replace("{data}", StatToText(Managers.Data.UserSkill.GetSkillLevelData(SkillType, GetSkillLevel()).BattleDatas[i], i));
+            result.Add(TextUtility.RelpaceKeyToValue(text));
+        }
+        return result;
+    }
+
+
+    string StatToText(float stat, int index)
+    {
+        switch (SkillType)
+        {
+            case SkillType.거인학살자: stat += 1; break;
+            case SkillType.태극스킬: stat += Managers.Data.Unit.UnitStatByFlag[new UnitFlags(0, index)].Damage; break;
+            case SkillType.흑의결속: stat += Managers.Data.Unit.UnitStatByFlag[new UnitFlags(7, index)].Damage; break;
+        }
+        if (stat >= 1000) return stat.ToString("#,##0");
+        else return stat.ToString();
     }
 }
